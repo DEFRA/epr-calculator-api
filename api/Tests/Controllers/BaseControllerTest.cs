83e2fb54b4a9418788f37b82ae30d1d1112ccb04
udaydefra <uday.2.denduluri.external@eviden.com>
2024-07-23 17:46:53 +0100

    Adding unit tests - UD - 408837

---
+++ api/Tests/Controllers/BaseControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using api.Controllers;
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using NUnit.Framework;
+
+namespace api.Tests.Controllers
+{
+    public class BaseControllerTest
+    {
+        protected ApplicationDBContext _dbContext;
+        protected DefaultParameterSettingController _controller;
+
+        protected static DbContextOptions _dbContextOptions = new DbContextOptionsBuilder<ApplicationDBContext>()
+            .UseInMemoryDatabase(databaseName: "PayCal")
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dbContext = new ApplicationDBContext(_dbContextOptions);
+            _dbContext.Database.EnsureCreated();
+
+            _controller = new DefaultParameterSettingController(_dbContext);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _dbContext.Database.EnsureDeleted();
+        }
+    }
+}
--- api/Tests/Controllers/DefaultParameterSettingControllerTest.cs
+++ api/Tests/Controllers/DefaultParameterSettingControllerTest.cs
@@ -1,44 +1,75 @@
-﻿using api.Controllers;
-using api.Data;
-using api.Dtos;
-using Microsoft.EntityFrameworkCore;
+﻿using api.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 
 namespace api.Tests.Controllers
 {
-    public class DefaultParameterSettingControllerTest
+    public class DefaultParameterSettingControllerTest : BaseControllerTest
     {
-        private ApplicationDBContext _dbContext;
-        private DefaultParameterSettingController _controller;
-
-        private static DbContextOptions _dbContextOptions = new DbContextOptionsBuilder<ApplicationDBContext>()
-            .UseInMemoryDatabase(databaseName: "PayCal")
-            .Options;
-
-        [OneTimeSetUp]
-        public void SetUp()
-        {
-            _dbContext = new ApplicationDBContext(_dbContextOptions);
-            _dbContext.Database.EnsureCreated();
-
-            _controller = new DefaultParameterSettingController(_dbContext);
-        }
+        private static string[] _uniqueReferences = {"BADEBT-P", "COMC-AL", "COMC-FC", "COMC-GL",
+                                                     "COMC-OT", "COMC-PC", "COMC-PL", "COMC-ST",
+                                                     "COMC-WD", "LAPC-ENG","LAPC-NIR", "LAPC-SCT",
+                                                    "LAPC-WLS", "LEVY-ENG", "LEVY-NIR", "LEVY-SCT", "LEVY-WLS",
+                                                    "LRET-AL", "LRET-FC", "LRET-GL", "LRET-OT",
+                                                    "LRET-PC", "LRET-PL", "LRET-ST", "LRET-WD", "MATT-AD",
+                                                    "MATT-AI", "MATT-PD", "MATT-PI", "SAOC-ENG", "SAOC-NIR", 
+                                                    "SAOC-SCT", "SAOC-WLS", "SCSC-ENG","SCSC-NIR", "SCSC-SCT",
+                                                    "SCSC-WLS", "TONT-AI", "TONT-DI", "TONT-PD","TONT-PI" };
 
         [Test]
-        public void CreateTest()
+        public void CreateTest_With41_Records()
         {
+            var schemeParameterTemplateValues = new List<SchemeParameterTemplateValue>();
+            foreach (var item in _uniqueReferences)
+            {
+                schemeParameterTemplateValues.Add(new SchemeParameterTemplateValue
+                {
+                    ParameterValue = 90,
+                    ParameterUniqueReferenceId = item
+                });
+            }
             var createDefaultParameterDto = new CreateDefaultParameterSettingDto
             {
                 ParameterYear = "2024-25",
-                SchemeParameterTemplateValues = new List<SchemeParameterTemplateValue>()
+                SchemeParameterTemplateValues = schemeParameterTemplateValues
             };
-            var actionResult = _controller.Create(createDefaultParameterDto);
+            var actionResult = _controller.Create(createDefaultParameterDto) as ObjectResult;
+            Assert.That(actionResult.StatusCode, Is.EqualTo(201));
+
+            Assert.That(_dbContext.DefaultParameterSettingDetail.Count(), Is.EqualTo(41));
+            Assert.That(_dbContext.DefaultParameterSettings.Count(), Is.EqualTo(1));
+            Assert.That(_dbContext.DefaultParameterTemplateMasterList.Count(), Is.EqualTo(41));
         }
 
-        [OneTimeTearDown]
-        public void TearDown()
+        [Test]
+        public void CreateTest_With41_Records_When_Existing_Updates()
         {
-            _dbContext.Database.EnsureDeleted();
+            var schemeParameterTemplateValues = new List<SchemeParameterTemplateValue>();
+            foreach (var item in _uniqueReferences)
+            {
+                schemeParameterTemplateValues.Add(new SchemeParameterTemplateValue
+                {
+                    ParameterValue = 90,
+                    ParameterUniqueReferenceId = item
+                });
+            }
+            var createDefaultParameterDto = new CreateDefaultParameterSettingDto
+            {
+                ParameterYear = "2024-25",
+                SchemeParameterTemplateValues = schemeParameterTemplateValues
+            };
+            var actionResult1 = _controller.Create(createDefaultParameterDto) as ObjectResult;
+            Assert.That(actionResult1.StatusCode, Is.EqualTo(201));
+
+            var actionResult2 = _controller.Create(createDefaultParameterDto) as ObjectResult;
+            Assert.That(actionResult2.StatusCode, Is.EqualTo(201));
+
+            Assert.That(_dbContext.DefaultParameterSettingDetail.Count(), Is.EqualTo(82));
+            Assert.That(_dbContext.DefaultParameterSettings.Count(), Is.EqualTo(2));
+            Assert.That(_dbContext.DefaultParameterTemplateMasterList.Count(), Is.EqualTo(41));
+
+            Assert.That(_dbContext.DefaultParameterSettingDetail.Count(x => x.DefaultParameterSettingMasterId == 2), Is.EqualTo(41));
+            Assert.That(_dbContext.DefaultParameterSettings.Count(a => a.EffectiveTo == null), Is.EqualTo(1));
         }
     }
 }
