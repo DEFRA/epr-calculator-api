de781b27dd3938fda09c807472394baf3cfad997
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-07-26 09:49:53 +0100

    Update Program.cs file

--- src/EPR.Calculator.API/Program.cs
+++ src/EPR.Calculator.API/Program.cs
@@ -3,10 +3,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.Development.json",
-        optional: true,
-        reloadOnChange: true);
-
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
