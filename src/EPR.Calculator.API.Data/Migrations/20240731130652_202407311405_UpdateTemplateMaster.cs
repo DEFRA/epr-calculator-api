2dfa76b34a854841069a2fedf502ff62db8f27bb
udaydefra <uday.2.denduluri.external@eviden.com>
2024-07-31 14:50:48 +0100

    Valiations 415482 - UD

---
+++ src/EPR.Calculator.API.Data/Migrations/20240731130652_202407311405_UpdateTemplateMaster.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202407311405_UpdateTemplateMaster : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "default_parameter_template_master",
+                columns: new[] { "parameter_unique_ref", "parameter_type", "parameter_category", "valid_Range_from", "valid_Range_to" },
+                values: new Object[,] {
+                                {
+                                    "TONT-AD", "Amount Decrease", "Tonnage change threshold", 0.00M, 999999999.99M
+                                }
+                }
+            );
+
+            migrationBuilder.UpdateData(
+                table: "default_parameter_setting_detail",
+                keyColumn: "parameter_unique_ref",
+                keyValue: "TONT-DI",
+                column: "parameter_unique_ref",
+                value: "TONT-AD"
+            );
+
+            migrationBuilder.DeleteData(
+                table: "default_parameter_template_master",
+                keyColumn: "parameter_unique_ref",
+                keyValue: "TONT-DI"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
