e851967eb6de7f7631bcb154538846f4da291583
udaydefra <uday.2.denduluri.external@eviden.com>
2024-08-15 21:36:20 +0100

    Adding appinsights - UD

--- src/EPR.Calculator.API/Program.cs
+++ src/EPR.Calculator.API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateDefaultParameterSettingValidator>());
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
