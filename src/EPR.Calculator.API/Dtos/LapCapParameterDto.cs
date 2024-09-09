11deb3b901c35f9badb8a22633bb6fc87571971e
Rekha Devi <rekha.devi.external@eviden.com>
2024-09-09 17:29:04 +0100

    Core functionality is implemented for Lapcap Get data with creating Dto and Mapper classes

---
+++ src/EPR.Calculator.API/Dtos/LapCapParameterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.API.Dtos
+{
+    [ExcludeFromCodeCoverage]
+    public class LapCapParameterDto
+    {
+        public int Id { get; set; }
+        public string Year { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public int LapcapDataMasterId { get; set; }
+        public string LapcapTempUniqueRef { get; set; }
+        public string Country { get; set; }
+
+        public string Material { get; set; }
+
+        public decimal TotalCost { get; set; }
+    }
+}
---
+++ src/EPR.Calculator.API/Mappers/LAPCAPParameterSettingMapper.cs
@@ -0,0 +1,34 @@
+﻿using EPR.Calculator.API.Data.DataModels;
+using EPR.Calculator.API.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPR.Calculator.API.Mappers
+{
+    public static class LAPCAPParameterSettingMapper
+    {
+        public static List<LapCapParameterDto> Map(LapcapDataMaster defaultParameterSettingMaster, DbSet<LapcapDataTemplateMaster> defaultParameterTemplate)
+        {
+            var result = new List<LapCapParameterDto>();
+
+            foreach (var item in defaultParameterSettingMaster.Details)
+            {
+                var selectedTemplate = defaultParameterTemplate.Single(x => x.UniqueReference == item.UniqueReference);
+                var data = new LapCapParameterDto
+                {
+                    Id = item.Id,
+                    Year = defaultParameterSettingMaster.Year,
+                    CreatedBy = defaultParameterSettingMaster.CreatedBy,
+                    CreatedAt = defaultParameterSettingMaster.CreatedAt,
+                    LapcapDataMasterId = item.Id,
+                    LapcapTempUniqueRef = item.UniqueReference,
+                    Country = selectedTemplate.Country,
+                    Material = selectedTemplate.Material,
+                    TotalCost = item.TotalCost
+                };
+
+                result.Add(data);
+            }
+            return result;
+        }
+    }
+}
