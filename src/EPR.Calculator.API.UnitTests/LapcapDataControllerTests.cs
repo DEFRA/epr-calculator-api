7fc579f7532a307e214093b48d5cf491d3dae7e5
Rekha Devi <rekha.devi.external@eviden.com>
2024-09-10 12:09:09 +0100

    Unite testing done for Lapcap Controller file

--- src/EPR.Calculator.API.UnitTests/LapcapDataControllerTests.cs
+++ src/EPR.Calculator.API.UnitTests/LapcapDataControllerTests.cs
@@ -1,7 +1,9 @@
 ﻿using api.Tests.Controllers;
 using EPR.Calculator.API.Constants;
 using EPR.Calculator.API.Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace EPR.Calculator.API.UnitTests
@@ -17,6 +19,68 @@ namespace EPR.Calculator.API.UnitTests
 
             Assert.AreEqual(LapcapDataUniqueReferences.UniqueReferences.Length, dbContext?.LapcapDataDetail.Count());
             Assert.AreEqual(1, dbContext?.LapcapDataMaster.Count());
+        }
+
+        //GET API
+        [TestMethod]
+        public void Get_RequestOkResult_WithLapCapParametersDto_WhenDataExist()
+        {
+            DataPostCall();
+
+            var tempdateData = new LapCapParameterDto()
+            {
+                Id = 1,
+                Year = "2024-25",
+                LapcapDataMasterId = 1,
+
+                LapcapTempUniqueRef = "ENG-AL",
+                CreatedBy = "Testuser",
+                CreatedAt = DateTime.Now,
+
+                Country = "England",
+                Material = "Aluminium",
+                TotalCost = 20m,
+            };
+
+            //Act
+            var actionResult1 = lapcapDataController?.Get("2024-25") as ObjectResult;
+
+            //Assert
+            var okResult = actionResult1 as ObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(okResult.StatusCode, 200);
+
+            var actionResul2 = okResult.Value as List<LapCapParameterDto>;
+            Assert.AreEqual(actionResul2?.Count, LapcapDataUniqueReferences.UniqueReferences.Length);
+
+            Assert.AreEqual(tempdateData.Id, actionResul2?[0].Id);
+            Assert.AreEqual(tempdateData.TotalCost, actionResul2?[0].TotalCost);
+            Assert.AreEqual(tempdateData.LapcapTempUniqueRef, actionResul2?[0].LapcapTempUniqueRef);
+        }
+
+        [TestMethod]
+        public void Get_InvalidModelState_ReturnsBadRequest()
+        {
+            lapcapDataController?.ModelState.AddModelError("parameterYear", "Invalid year");
+            //Act
+            var result = lapcapDataController?.Get("2024") as ObjectResult;
+            //Assert
+            var okResult = result as ObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(StatusCodes.Status400BadRequest, okResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void Get_NoDataForYear_ReturnsNotFound()
+        {
+            DataPostCall();
+
+            //Act
+            var result = lapcapDataController?.Get("2028-25") as ObjectResult;
+            //Assert
+            var okResult = result as ObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(StatusCodes.Status404NotFound, okResult.StatusCode);
         }
 
         public ObjectResult? DataPostCall()
@@ -35,4 +99,5 @@ namespace EPR.Calculator.API.UnitTests
             return actionResult;
         }
     }
+
 }
