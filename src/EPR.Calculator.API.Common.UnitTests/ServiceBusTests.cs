289338c879fd60960d517da1610127f42cec208b
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-09-22 15:31:27 +0100

    444833 Spike Azure Service Bus

---
+++ src/EPR.Calculator.API.Common.UnitTests/ServiceBusTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EPR.Calculator.API.Common.UnitTests
+{
+    [TestClass]
+    public class ServiceBusTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
