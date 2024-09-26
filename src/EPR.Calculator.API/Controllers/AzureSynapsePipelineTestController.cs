8c005fd05352cd01058720f43e37131b79461e7c
Tim Powell <timothy.powell@eviden.com>
2024-09-26 10:51:03 +0100

    Add controller for testing Azure Synaptics pipelines

---
+++ src/EPR.Calculator.API/Controllers/AzureSynapsePipelineTestController.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Analytics.Synapse.Artifacts;
+using Azure.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.API.Controllers
+{
+    
+    public class AzureSynapsePipelineTestController(IConfiguration configuration) : ControllerBase
+    {
+        private string PipelineUrl { get; init; } = configuration.GetSection("AzureSynapse")["PipelineUrl"]
+            ?? string.Empty;
+
+        private string PipelineName { get; init; } = configuration.GetSection("AzureSynapse")["PipelineName"]
+            ?? string.Empty;
+
+        [Route("AzureSynapseTest")]
+        [HttpGet]
+        public async Task<IActionResult> GetPipeline()
+        {
+#if DEBUG
+            var credentials = new DefaultAzureCredential();
+#else
+            var TokenCredential credentials = What do we need to use for credentials in prod? Managed identity maybe?
+#endif
+
+            var pipelineClient = new PipelineClient(
+                new Uri(this.PipelineUrl),
+                credentials);
+
+            try
+            {
+                var result = await pipelineClient.GetPipelineAsync(
+                this.PipelineName);
+
+                return Ok(result.GetRawResponse().ToString());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [Route("AzureSynapseTest")]
+        [HttpPost]
+        public async Task<IActionResult> PostPipeline(Dictionary<string,object> parameters)
+        {
+#if DEBUG
+            var credentials = new InteractiveBrowserCredential();
+#else
+                        var TokenCredential credentials = new ManagedIdentityCredential();
+#endif
+
+            var pipelineClient = new PipelineClient(
+                new Uri(this.PipelineUrl),
+                credentials);
+
+            try
+            {
+                var result = await pipelineClient.CreatePipelineRunAsync(
+                this.PipelineName,
+                parameters: parameters);
+
+                return Ok(result.GetRawResponse().ToString());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+            return Ok();
+        }
+    }
+}
