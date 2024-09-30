d4c666feef3faff8ec1f8451c582c9ae86482441
rekhadhankhar2018 <42934997+rekhadhankhar2018@users.noreply.github.com>
2024-09-30 13:29:46 +0100

    Feature/444802 lapcap update error message format for file contents error page (#43)
    * modify the new error introduce for LApcap data upload
    
    * remove unnecessary lines
    
    * build time unit test error fixed.
    
    * fixes doen for PR
    
    * formating fixed
    
    * Run time build test error fixes
    
    ---------
    
    Co-authored-by: Rekha Devi <rekha.devi.external@eviden.com>
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@eviden.com>

--- src/EPR.Calculator.API/Validators/LapcapDataValidator.cs
+++ src/EPR.Calculator.API/Validators/LapcapDataValidator.cs
@@ -1,13 +1,14 @@
 ﻿using EPR.Calculator.API.Data;
 using EPR.Calculator.API.Dtos;
 using EPR.Calculator.API.Utils;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EPR.Calculator.API.Validators
 {
     public class LapcapDataValidator : ILapcapDataValidator
     {
         private readonly ApplicationDBContext context;
-        public LapcapDataValidator(ApplicationDBContext context) 
+        public LapcapDataValidator(ApplicationDBContext context)
         {
             this.context = context;
         }
@@ -32,11 +33,11 @@ namespace EPR.Calculator.API.Validators
 
                 if (matchingCount == 0)
                 {
-                    errorMessage = $"Enter the lapcap data for {country} and {material}";
+                    errorMessage = $"Enter the total costs for {material} in {country}";
                 }
-                else if (matchingCount > 1) 
+                else if (matchingCount > 1)
                 {
-                    errorMessage = $"Expecting only One with {country} and {material}";
+                    errorMessage = $"You have entered the total costs for {material} in {country} more than once";
                 }
                 else
                 {
@@ -45,22 +46,22 @@ namespace EPR.Calculator.API.Validators
                     var totalCostStr = data.TotalCost;
                     if (string.IsNullOrEmpty(totalCostStr))
                     {
-                        errorMessage = $"Enter the lapcap data for {country} and {material}";
+                        errorMessage = $"Enter the total costs for {material} in {country}";
                     }
                     else if (decimal.TryParse(totalCostStr, out totalCostValue))
                     {
                         if (totalCostValue < lapcapTemplate.TotalCostFrom ||
                             totalCostValue > lapcapTemplate.TotalCostTo)
-                        {
-                            errorMessage = $"Total cost for {country} and {material} should be between {totalCostFrom} and {totalCostTo}";
+                        {                          
+                            errorMessage = $"Total costs for {material} in {country} must be between £{Convert.ToInt16(totalCostFrom)} and £{totalCostTo.ToString("#,##0.00")}";
                         }
                     }
                     else
                     {
-                        errorMessage = $"Enter the lapcap data for {country} and {material}";
+                        errorMessage = $"Total costs for {material} can only include numbers, commas and decimal points";
                     }
                 }
-                if(!string.IsNullOrEmpty(errorMessage))
+                if (!string.IsNullOrEmpty(errorMessage))
                 {
                     var errorDto = Util.CreateLapcapDataErrorDto(country, material, errorMessage, string.Empty, uniqueRef);
                     validationResult.Errors.Add(errorDto);
