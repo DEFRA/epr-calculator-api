f29911fe99e8cf047904c0287eb2a0dc6f993456
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-10-03 17:02:26 +0100

    449361 Create API to initiate calculator run

---
+++ src/EPR.Calculator.API.Common.UnitTests/ServiceBusTests.cs
@@ -0,0 +1,23 @@
+namespace EPR.Calculator.API.Common.UnitTests
+{
+    [TestClass]
+    public class ServiceBusTests
+    {
+        //[TestMethod]
+        //public async Task SendMessage_ReturnErrorIfNoConnectionString()
+        //{
+        //    try
+        //    {
+        //        var message = new CalculatorRunMessage { CalculatorRunId = "123450" };
+        //        await ServiceBus.ServiceBus.SendMessage(string.Empty, "TestQueue", message);
+        //    }
+        //    catch (ServiceBusException exception)
+        //    {
+        //        Assert.IsTrue(exception.Message.Contains("ServiceBusClient: Connection string not provided."));
+        //        Assert.AreEqual(ServiceBusFailureReason.ServiceCommunicationProblem, exception.Reason);
+        //    }
+        //}
+
+        // TO DO: Write more coverage during actual implementation
+    }
+}
