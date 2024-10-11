fb9e17816e01702bc0e459513825b4215d34f49e
Mazar Shaik <mazar.shaik.external@eviden.com>
2024-10-11 15:46:00 +0100

    Fix for System.InvalidOperationException in program.cs

--- src/EPR.Calculator.API/Program.cs
+++ src/EPR.Calculator.API/Program.cs
@@ -33,7 +33,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseExceptionHandler();
+app.UseExceptionHandler( _ => { });
 app.UseHttpsRedirection();
 app.MapControllers();
 
