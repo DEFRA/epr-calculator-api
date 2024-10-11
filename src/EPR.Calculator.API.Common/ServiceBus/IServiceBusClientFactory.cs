f7960d28bf542fbc0cb4deb1da25eaefd7aa7741
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-10-11 10:53:24 +0100

    449361 Create API to initiate calculator run

--- src/EPR.Calculator.API.Common/ServiceBus/IServiceBusClientFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Azure.Messaging.ServiceBus;
-
-namespace EPR.Calculator.API.Common.ServiceBus
-{
-    public interface IServiceBusClientFactory
-    {
-        public ServiceBusClient GetServiceBusClient(string connectionString, int retryCount, int retryPeriod);
-    }
-}
