143fc9ffadf310bb3901d6565751f0a1337018f2
udaydefra <uday.2.denduluri.external@eviden.com>
2024-10-18 11:59:27 +0100

    Temp changes

---
+++ src/EPR.Calculator.API/Controllers/CalculatorInternalController.cs
@@ -0,0 +1,47 @@
+﻿using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.API.Controllers
+{
+    [Route("v1/internal")]
+    [ApiController]
+    public class CalculatorInternalController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+
+        public CalculatorInternalController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        [Route("rpdStatus")]
+        public IActionResult UpdateRpdStatus([FromBody] UpdateRpdStatus request)
+        {
+            var runId = request.RunId;
+
+            var calcRunExist = this.context.CalculatorRuns.Any(run => run.Id == runId);
+            if (!calcRunExist)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest, $"Calculator Run for {runId} is missing");
+            }
+
+            var pomDataExists = this.context.PomData.Any();
+            if (!pomDataExists)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, $"PomData for {runId} is missing");
+            }
+
+            var organisationDataExists = this.context.OrganisationData.Any();
+            if (!organisationDataExists)
+            {
+                return StatusCode(StatusCodes.Status422UnprocessableEntity, $"OrganisationData for {runId} is missing");
+            }
+
+
+
+            return new ObjectResult(null) { StatusCode = StatusCodes.Status202Accepted };
+        }
+    }
+}
---
+++ src/EPR.Calculator.API/Dtos/UpdateRpdStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.Calculator.API.Dtos
+{
+    [ExcludeFromCodeCoverage]
+    public class UpdateRpdStatus
+    {
+        public required int RunId { get; set; }
+
+        public required string UpdateBy { get; set; }
+
+        public required bool isSuccessful { get; set; }
+    }
+}
