12a839e4ef9d5d01ccec80d86b662fd7dff062db
Santhosh Kumar <skumar.hpa@gmail.com>
2024-10-24 15:51:51 +0100

    updated the run classification

---
+++ src/EPR.Calculator.API/Enums/RunClassification.cs
@@ -0,0 +1,11 @@
+﻿namespace EPR.Calculator.API.Enums
+{
+    public enum RunClassification
+    {
+        InTheQueue = 1,
+        Running = 2,
+        Unclassified = 3,
+        Play = 4,
+        Error = 5
+    }
+}
