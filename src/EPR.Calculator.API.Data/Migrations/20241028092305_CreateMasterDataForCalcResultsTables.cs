89386b45e4acfc1803b11d446916065bffe3e5d9
udaydefra <uday.2.denduluri.external@eviden.com>
2024-10-28 09:24:42 +0000

    Adding Blank Migration for CalcResultsTable

---
+++ src/EPR.Calculator.API.Data/Migrations/20241028092305_CreateMasterDataForCalcResultsTables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMasterDataForCalcResultsTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
