5da5e89e7dc38bdc9cbca4eb38efefaf5e7e4ef2
udaydefra <uday.2.denduluri.external@eviden.com>
2024-10-30 15:38:06 +0000

    Adding CalcResultParameterCommunicationCost - 463833

--- src/EPR.Calculator.API/Models/CalcResult.cs
+++ src/EPR.Calculator.API/Models/CalcResult.cs
@@ -7,5 +7,7 @@
         public required CalcResultLapcapData CalcResultLapcapData { get; set; }
 
         public required CalcResultLateReportingTonnage CalcResultLateReportingTonnageDetail { get; set; }
+
+        public required CalcResultParameterCommunicationCost CalcResultParameterCommunicationCost { get; set; }
     }
 }
---
+++ src/EPR.Calculator.API/Models/CalcResultParameterCommunicationCost.cs
@@ -0,0 +1,7 @@
+﻿namespace EPR.Calculator.API.Models
+{
+    public class CalcResultParameterCommunicationCost
+    {
+        public IEnumerable<CalcResultParameterCommunicationCostDetail> CalcResultParameterCommunicationCostDetails { get; set; }
+    }
+}
