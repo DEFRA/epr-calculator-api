87c5de7fcf3b0ba77c555d4c069905b1b36e963e
Rekha Devi <rekha.devi.external@eviden.com>
2024-11-04 09:23:49 +0000

    Added Validation

--- src/EPR.Calculator.API/Validators/CreateLapcapDataValidator.cs
+++ src/EPR.Calculator.API/Validators/CreateLapcapDataValidator.cs
@@ -11,6 +11,8 @@ namespace EPR.Calculator.API.Validators
             RuleFor(x => x.ParameterYear).NotEmpty().WithMessage((ErrorMessages.YearRequired));
             RuleFor(x => x.LapcapDataTemplateValues).NotNull().Must(x => x.Count() == LapcapDataUniqueReferences.UniqueReferences.Length)
                 .WithMessage((ErrorMessages.LapcapDataTemplateValuesMissing));
+            RuleFor(x => x.LapcapFileName).NotEmpty().WithMessage(ErrorMessages.FileNameRequired);
+            RuleFor(x => x.LapcapFileName).MaximumLength(256).WithMessage(ErrorMessages.MaxFileNameLength);
         }
     }
 }
