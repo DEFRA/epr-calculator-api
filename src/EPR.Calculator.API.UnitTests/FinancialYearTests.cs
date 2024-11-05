aaac91f22c518970df86a567877b8d1e8ebe3b47
Sunita Gupta <sunita.gupta.external@eviden.com>
2024-11-05 14:22:10 +0000

    Added FinancialYear class

---
+++ src/EPR.Calculator.API.UnitTests/FinancialYearTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EPR.Calculator.API.Utils;
+
+namespace EPR.Calculator.API.UnitTests
+{
+    [TestClass]
+    public class FinancialYearTests
+    {
+        [TestMethod]
+        public void FinancialYear_Value_ShouldReturnCorrectDateTime()
+        {
+            var date = new DateTime(2024, 1, 1);
+            var financialYear = new FinancialYear(date);
+
+            var result = financialYear.Value;
+
+            Assert.AreEqual(date, result);
+        }
+
+        [TestMethod]
+        public void ToCalendarYear_ShouldReturnPreviousYear()
+        {
+            var date = new DateTime(2024, 1, 1);
+            var financialYear = new FinancialYear(date);
+
+            var result = financialYear.ToCalendarYear();
+
+            Assert.AreEqual(new DateTime(2023, 1, 1), result);
+        }
+
+        [TestMethod]
+        public void Parse_ValidString_ShouldReturnFinancialYear()
+        {
+            var input = "2024-25";
+
+            var result = FinancialYear.Parse(input);
+
+            Assert.AreEqual(new DateTime(2024, 1, 1), result.Value);
+        }
+
+        [TestMethod]
+        [DataRow("2024-25", "2024")]
+        [DataRow("2023-24", "2023")]
+        [DataRow("2022-23", "2022")]
+        public void FinancialYearAsString_ValidString_ShouldReturnFirstYear(string financialYear, string expectedFinancialYear)
+        {
+            var result = FinancialYear.FinancialYearAsString(financialYear);
+            Assert.AreEqual(expectedFinancialYear, result);
+        }
+
+        [TestMethod]
+        [DataRow("2024")]
+        [DataRow("24-25")]
+        [DataRow("2024-2025")]
+        [DataRow("abcd-efgh")]
+        public void FinancialYearAsString_InvalidString_ShouldThrowFormatException(string financialYear)
+        {
+            var exception = Assert.ThrowsException<FormatException>(() => FinancialYear.FinancialYearAsString(financialYear));
+            Assert.AreEqual("Financial year format is invalid. Expected format is 'YYYY-YY'.", exception.Message);
+        }
+
+        [TestMethod]
+        [DataRow("2024-25", "2023")]
+        [DataRow("2023-24", "2022")]
+        [DataRow("2022-23", "2021")]
+        public void ToCalendarYearAsString_ValidString_ShouldReturnPreviousYearAsString(string financialYear, string expectedCalendarYear)
+        {
+            var result = FinancialYear.ToCalendarYearAsString(financialYear);
+            Assert.AreEqual(expectedCalendarYear, result);
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(" ")]
+        public void ToCalendarYearAsString_NullOrEmptyString_ShouldThrowArgumentException(string financialYear)
+        {
+            var exception = Assert.ThrowsException<ArgumentException>(() => FinancialYear.ToCalendarYearAsString(financialYear));
+            Assert.AreEqual("Financial year cannot be null or empty (Parameter 'value')", exception.Message);
+        }
+    }
+}
---
+++ src/EPR.Calculator.API/Utils/FinancialYear.cs
@@ -0,0 +1,71 @@
+﻿using System.Globalization;
+
+namespace EPR.Calculator.API.Utils
+{
+    public record FinancialYear(DateTime value)
+    {
+        /// <summary>
+        /// Gets the financial year.
+        /// </summary>
+        public DateTime Value { get; } = value;
+
+        /// <summary>
+        /// Converts the financial year to the previous calendar year.
+        /// </summary>
+        /// <returns>The previous calendar year as a <see cref="DateTime"/>.</returns>
+        public DateTime ToCalendarYear() => new (this.value.Year - 1, 1, 1);
+
+        /// <summary>
+        /// Parses a financial year from a string.
+        /// </summary>
+        /// <param name="value">The year string to parse, in the format "YYYY-YY".</param>
+        /// <returns>A <see cref="FinancialYear"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown when the format is invalid.</exception>
+        public static FinancialYear Parse(string value)
+        {
+            try
+            {
+                return new FinancialYear(
+                    DateTime.ParseExact(value.Substring(0, 4), "yyyy", CultureInfo.InvariantCulture));
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException("Invalid format. Please use the format 'YYYY-YY'.");
+            }
+        }
+
+        /// <summary>
+        /// Extracts the first year from a financial year string in the format "YYYY-YY".
+        /// </summary>
+        /// <param name="value">The financial year string to parse, in the format "YYYY-YY".</param>
+        /// <returns>The first year as a string.</returns>
+        /// <exception cref="FormatException">Thrown when the format is invalid.</exception>
+        public static string FinancialYearAsString(string value)
+        {
+            var years = value.Split('-');
+            if (years.Length != 2 || !int.TryParse(years[0], out int year) || years[0].Length != 4 || years[1].Length != 2)
+            {
+                throw new FormatException("Financial year format is invalid. Expected format is 'YYYY-YY'.");
+            }
+
+            return years[0];
+        }
+
+        /// <summary>
+        /// Converts a financial year string to the previous calendar year as a string.
+        /// </summary>
+        /// <param name="value">The financial year string to convert, in the format "YYYY-YY".</param>
+        /// <returns>The previous calendar year as a string.</returns>
+        /// <exception cref="ArgumentException">Thrown when the financial year string is null or empty.</exception>
+        public static string ToCalendarYearAsString(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Financial year cannot be null or empty", nameof(value));
+            }
+
+            int year = int.Parse(FinancialYearAsString(value));
+            return (year - 1).ToString();
+        }
+    }
+}
