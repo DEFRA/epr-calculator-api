31fe93be35921709d37f508ccab1172ed891a188
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-11-06 12:25:21 +0000

    Update TransposePomAndOrgDataService.cs

--- src/EPR.Calculator.API/Services/TransposePomAndOrgDataService.cs
+++ src/EPR.Calculator.API/Services/TransposePomAndOrgDataService.cs
@@ -26,24 +26,35 @@ namespace EPR.Calculator.API.Services
                 {
                     try
                     {
-                        var producerDetails = new List<ProducerDetail>();
+                        // Initialise the producerReportedMaterials
                         var producerReportedMaterials = new List<ProducerReportedMaterial>();
 
-                        var pomDataDetails = context.CalculatorRunPomDataDetails.Where(pdd => pdd.CalculatorRunPomDataMasterId == (int)calculatorRun.CalculatorRunPomDataMasterId).ToList();
+                        // Get the calculator run organisation data master record based on the CalculatorRunOrganisationDataMasterId
+                        // from the calculator run table
+                        var organisationDataMaster = context.CalculatorRunOrganisationDataMaster.Single(odm => odm.Id == calculatorRun.CalculatorRunOrganisationDataMasterId);
 
-                        foreach (var pom in pomDataDetails)
+                        // Get the calculator run organisation data details as we need the organisation name
+                        var organisationDataDetails = context.CalculatorRunOrganisationDataDetails.Where
+                            (
+                                odd => odd.CalculatorRunOrganisationDataMasterId == organisationDataMaster.Id
+                            ).ToList();
+
+                        // Get the calculator run pom data details related to the calculator run pom data master
+                        var calculatorRunPomDataDetails = context.CalculatorRunPomDataDetails.Where(pdd => pdd.CalculatorRunPomDataMasterId == (int)calculatorRun.CalculatorRunPomDataMasterId).ToList();
+                        
+                        // Loop through the calculator run pom data details and
+                        // populate the producerDetails and producerReportedMaterials
+                        foreach (var pom in calculatorRunPomDataDetails)
                         {
+                            // Proceed further only if the organisation id has a value
+                            // TO DO: We have to record if the organisation id is null in a separate table post Dec 2024
                             if (pom.OrganisationId.HasValue)
                             {
-                                var organisationDataMaster = context.CalculatorRunOrganisationDataMaster.Single(odm => odm.Id == calculatorRun.CalculatorRunOrganisationDataMasterId);
-
-                                var organisationDataDetails = context.CalculatorRunOrganisationDataDetails.Where
-                                    (
-                                        odd => odd.CalculatorRunOrganisationDataMasterId == organisationDataMaster.Id
-                                    ).ToList();
-
+                                // Get the producer name
                                 var producerName = organisationDataDetails.Single(odd => odd.OrganisationId == pom.OrganisationId)?.OrganisationName;
 
+                                // Proceed further only if the organisation name has a value
+                                // TO DO: We have to record if the organisation name is null in a separate table post Dec 2024
                                 if (producerName != null)
                                 {
                                     var producerDetail = new ProducerDetail
@@ -55,8 +66,7 @@ namespace EPR.Calculator.API.Services
                                         CalculatorRun = calculatorRun
                                     };
 
-                                    producerDetails.Add(producerDetail);
-
+                                    // Add producer detail record to the database context
                                     context.ProducerDetail.Add(producerDetail);
 
                                     var material = materials.Single(m => m.Code == pom.PackagingMaterial);
@@ -73,15 +83,20 @@ namespace EPR.Calculator.API.Services
                                             PackagingTonnage = (decimal)pom.PackagingMaterialWeight / 1000,
                                         };
 
+                                        // Populate the producer reported material list
                                         producerReportedMaterials.Add(producerReportedMaterial);
                                     }
                                 }
                             }
                         }
 
+                        // Add the list of producer reported materials to the database context
                         context.ProducerReportedMaterial.AddRange(producerReportedMaterials);
+
+                        // Apply the database changes
                         context.SaveChanges();
 
+                        // Success, commit transaction
                         transaction.Commit();
                     }
                     catch (Exception)
