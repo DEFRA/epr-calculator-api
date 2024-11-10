3195e91377995fd5903bf26ab400ae9e218c6f89
udaydefra <uday.2.denduluri.external@eviden.com>
2024-11-10 11:10:57 +0000

    Other Parameters Cost

--- src/EPR.Calculator.API/Builder/ParametersOther/CalcResultParameterOtherCostBuilder.cs
+++ src/EPR.Calculator.API/Builder/ParametersOther/CalcResultParameterOtherCostBuilder.cs
@@ -1,13 +1,66 @@
-﻿using EPR.Calculator.API.Dtos;
+﻿using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Dtos;
 using EPR.Calculator.API.Models;
 
 namespace EPR.Calculator.API.Builder.ParametersOther
 {
     public class CalcResultParameterOtherCostBuilder : ICalcResultParameterOtherCostBuilder
     {
+        private const string SchemeAdminOperatingCost = "Scheme administrator operating costs";
+        private readonly ApplicationDBContext context;
+        public CalcResultParameterOtherCostBuilder(ApplicationDBContext context) 
+        {
+            this.context = context;
+        }
+
         public CalcResultParameterOtherCost Construct(CalcResultsRequestDto resultsRequestDto)
         {
-            return new CalcResultParameterOtherCost();
+            var results = (from run in context.CalculatorRuns
+                           join defaultMaster in context.DefaultParameterSettings on run.DefaultParameterSettingMasterId equals defaultMaster.Id
+                           join defaultDetail in context.DefaultParameterSettingDetail on defaultMaster.Id equals defaultDetail.DefaultParameterSettingMasterId
+                           join defaultTemplate in context.DefaultParameterTemplateMasterList on defaultDetail.ParameterUniqueReferenceId equals defaultTemplate.ParameterUniqueReferenceId
+                           where run.Id == resultsRequestDto.RunId
+                           select new DefaultParamResultsClass
+                           {
+                               ParameterValue = defaultDetail.ParameterValue,
+                               ParameterCategory = defaultTemplate.ParameterCategory,
+                               ParameterType = defaultTemplate.ParameterType,
+                           }).ToList();
+
+            var schemeAdminCosts = results.Where(x => x.ParameterCategory == SchemeAdminOperatingCost);
+
+            var other = new CalcResultParameterOtherCost();
+
+            other.SaOperatingCost = GetOtherSaOperatingCost(schemeAdminCosts);
+
+            return other;
+        }
+
+        private static IEnumerable<CalcResultParameterOtherCostDetail> GetOtherSaOperatingCost(IEnumerable<DefaultParamResultsClass> schemeAdminCosts)
+        {
+            var details = new List<CalcResultParameterOtherCostDetail>();
+
+            var header = new CalcResultParameterOtherCostDetail
+            {
+                England = "England",
+                Wales = "Wales",
+                Scotland = "Scotland",
+                NorthernIreland = "Northern Ireland"
+            };
+            details.Add(header);
+
+            var saOperatingCost = new CalcResultParameterOtherCostDetail
+            {
+                Name = "3 SA Operating Costs",
+                EnglandValue = schemeAdminCosts.Single(cost => cost.ParameterCategory == "England").ParameterValue,
+                NorthernIrelandValue = schemeAdminCosts.Single(cost => cost.ParameterCategory == "Northern Ireland").ParameterValue,
+                ScotlandValue = schemeAdminCosts.Single(cost => cost.ParameterCategory == "Scotland").ParameterValue,
+                WalesValue = schemeAdminCosts.Single(cost => cost.ParameterCategory == "Wales").ParameterValue,
+                OrderId = 2
+            };
+            saOperatingCost.TotalValue = saOperatingCost.EnglandValue + saOperatingCost.ScotlandValue + saOperatingCost.WalesValue + saOperatingCost.NorthernIrelandValue;
+            details.Add(saOperatingCost);
+            return details;
         }
     }
 }
--- src/EPR.Calculator.API/Models/CalcResultParameterOtherCostDetail.cs
+++ src/EPR.Calculator.API/Models/CalcResultParameterOtherCostDetail.cs
@@ -8,6 +8,11 @@
         public string Scotland { get; set; }
         public string NorthernIreland { get; set; }
         public string Total { get; set; }
+        public decimal EnglandValue { get; set; }
+        public decimal WalesValue { get; set; }
+        public decimal ScotlandValue { get; set; }
+        public decimal NorthernIrelandValue { get; set; }
+        public decimal TotalValue { get; set; }
         public int OrderId { get; set; }
     }
 }
