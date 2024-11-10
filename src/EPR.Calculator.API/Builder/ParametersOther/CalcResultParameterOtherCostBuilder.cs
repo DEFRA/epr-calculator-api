16908de6596c9428ddfb06f4942407ea0e7a0a47
udaydefra <uday.2.denduluri.external@eviden.com>
2024-11-10 13:30:35 +0000

    Adding Bad Debt provision

--- src/EPR.Calculator.API/Builder/ParametersOther/CalcResultParameterOtherCostBuilder.cs
+++ src/EPR.Calculator.API/Builder/ParametersOther/CalcResultParameterOtherCostBuilder.cs
@@ -8,6 +8,12 @@ namespace EPR.Calculator.API.Builder.ParametersOther
     {
         private const string SchemeAdminOperatingCost = "Scheme administrator operating costs";
         private const string LaPrepCharge = "Local authority data preparation costs";
+        private const string SaOperatingCostHeader = "3 SA Operating Costs";
+        private const string LaDataPrepChargeHeader = "4 LA Data Prep Charge";
+        private const string SchemeSetupCost = "Scheme setup costs";
+        private const string SchemeSetupYearlyCostHeader = "5 Scheme set up cost Yearly Cost";
+        private const string BadDebtProvision = "Bad debt provision";
+        private const string BadDebtProvisionHeader = "6 Bad debt provision";
         private readonly ApplicationDBContext context;
         public CalcResultParameterOtherCostBuilder(ApplicationDBContext context) 
         {
@@ -42,18 +48,43 @@ namespace EPR.Calculator.API.Builder.ParametersOther
             };
             saDetails.Add(header);
 
-            saDetails.Add(GetLaPrepCharge("3 SA Operating Costs", 2, schemeAdminCosts));
+            saDetails.Add(GetPrepCharge(SaOperatingCostHeader, 2, schemeAdminCosts));
             other.SaOperatingCost = saDetails;
 
             var lapPrepCharges = results.Where(x => x.ParameterType == LaPrepCharge);
             var laDataPrepCharges = new List<CalcResultParameterOtherCostDetail>();
-            laDataPrepCharges.Add(GetLaPrepCharge("4 LA Data Prep Charge", 1, lapPrepCharges));
+            var laDataPrep = GetPrepCharge(LaDataPrepChargeHeader, 1, lapPrepCharges);
+            laDataPrepCharges.Add(laDataPrep);
+            laDataPrepCharges.Add(GetCountryApportionment(laDataPrep));
             other.Details = laDataPrepCharges;
+
+            var schemeSetUpCharges = results.Where(x => x.ParameterType == SchemeSetupCost);
+            var schemeSetupCharge = GetPrepCharge(SchemeSetupYearlyCostHeader, 1, lapPrepCharges);
+            other.SchemeSetupCost = schemeSetupCharge;
+
+            var badDebtValue = results.Single(x => x.ParameterType == BadDebtProvision).ParameterValue;
+            other.BadDebtProvision = new KeyValuePair<string, string> (BadDebtProvisionHeader, $"{badDebtValue}%");
 
             return other;
         }
 
-        private static CalcResultParameterOtherCostDetail GetLaPrepCharge(string name, int orderId, IEnumerable<DefaultParamResultsClass> lapPrepCharges)
+        private static CalcResultParameterOtherCostDetail GetCountryApportionment(CalcResultParameterOtherCostDetail laDataPrep)
+        {
+            var otherCostDetail = new CalcResultParameterOtherCostDetail
+            {
+                Name = "4 Country Apportionment",
+                EnglandValue = (laDataPrep.EnglandValue / laDataPrep.TotalValue) * 100,
+                NorthernIrelandValue = (laDataPrep.NorthernIrelandValue / laDataPrep.TotalValue) * 100,
+                ScotlandValue = (laDataPrep.ScotlandValue / laDataPrep.TotalValue) * 100,
+                WalesValue = (laDataPrep.WalesValue / laDataPrep.TotalValue) * 100,
+                OrderId = 2,
+                TotalValue = 100M
+            };
+            
+            return otherCostDetail;
+        }
+
+        private static CalcResultParameterOtherCostDetail GetPrepCharge(string name, int orderId, IEnumerable<DefaultParamResultsClass> lapPrepCharges)
         {
             var otherCostDetail = new CalcResultParameterOtherCostDetail
             {
