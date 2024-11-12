d9a6ec01718f77c48665f2ee549aac82e00e51a9
Santhosh Kumar <skumar.hpa@gmail.com>
2024-11-12 14:55:55 +0000

    updated constant

--- src/EPR.Calculator.API/Constants/CommonConstants.cs
+++ src/EPR.Calculator.API/Constants/CommonConstants.cs
@@ -10,7 +10,7 @@ namespace EPR.Calculator.API.Constants
         public const string Scotland = "Scotland";
         public const string NorthernIreland = "Northern Ireland";
         public const string Total = "Total";
-        public const string ProducerReportedHouseholdPackagingWasteTonnage = "Producer Reported  Household Packaging Waste Tonnage";
+        public const string ProducerReportedHouseholdPackagingWasteTonnage = "Producer Reported Household Packaging Waste Tonnage";
         public const string LateReportingTonnage = "Late Reporting Tonnage";
         public const string ProduceLateTonnage =   "Producer Reported Household Tonnage + Late Reporting Tonnage";
         public const string DisposalCostPricePerTonne = "Disposal Cost Price Per Tonne";
