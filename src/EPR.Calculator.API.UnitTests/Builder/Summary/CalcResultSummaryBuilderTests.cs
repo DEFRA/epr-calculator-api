006db7d863a56c86b6845fd8b670d0698b365af3
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-11-12 22:14:44 +0000

    Create CalcResultSummaryBuilderTests.cs

---
+++ src/EPR.Calculator.API.UnitTests/Builder/Summary/CalcResultSummaryBuilderTests.cs
@@ -0,0 +1,53 @@
+﻿using EPR.Calculator.API.Builder.LaDisposalCost;
+using EPR.Calculator.API.Builder.Summary;
+using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Tests.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPR.Calculator.API.UnitTests.Builder.Summary
+{
+    [TestClass]
+    public class CalcResultSummaryBuilderTests
+    {
+        private CalcResultSummaryBuilder builder;
+        private ApplicationDBContext dbContext;
+
+        [TestInitialize]
+        public void DataSetup()
+        {
+            var dbContextOptions = new DbContextOptionsBuilder<ApplicationDBContext>()
+                                    .UseInMemoryDatabase(databaseName: "PayCal")
+                                    .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+            dbContext = new ApplicationDBContext(dbContextOptions);
+            dbContext.Database.EnsureCreated();
+            dbContext.DefaultParameterTemplateMasterList.RemoveRange(dbContext.DefaultParameterTemplateMasterList);
+            dbContext.SaveChanges();
+            dbContext.DefaultParameterTemplateMasterList.AddRange(BaseControllerTest.GetDefaultParameterTemplateMasterData().ToList());
+            // dbContext.LapcapDataTemplateMaster.AddRange(BaseControllerTest.GetLapcapTemplateMasterData().ToList());
+            dbContext.SaveChanges();
+
+            builder = new CalcResultSummaryBuilder(dbContext);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            dbContext?.Database.EnsureDeleted();
+        }
+
+        [TestMethod]
+        public void Should_Construct()
+        {
+
+        }
+    }
+}
