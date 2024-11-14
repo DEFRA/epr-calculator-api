063b430045e608f181e25137886b8f5c7d24f12b
udaydefra <uday.2.denduluri.external@eviden.com>
2024-11-14 19:09:39 +0000

    CommsCostBuilder Changes - UD

--- src/EPR.Calculator.API/Builder/CommsCost/CalcResultCommsCost.cs
+++ src/EPR.Calculator.API/Builder/CommsCost/CalcResultCommsCost.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace EPR.Calculator.API.Builder.CommsCost
 {
@@ -7,50 +8,8 @@ namespace EPR.Calculator.API.Builder.CommsCost
     /// </summary>
     public class CalcResultCommsCost
     {
-        /// <summary>
-        /// The delimiter to use between fields in the report.
-        /// </summary>
-        public const char Delimiter = ',';
-
-        /// <summary>
-        /// Headers for the report - these go before the headers for each country.
-        /// </summary>
-        private IEnumerable<string> Headers1 { get; } =
-        [
-            "2a Comms Costs - by Material",
-        ];
-
-        /// <summary>
-        /// Headers for the report - these go after the headers for each country.
-        /// </summary>
-        private IEnumerable<string> Headers2 { get; } =
-        [
-            "Total",
-            "Producer Reported Household Packaging Waste Tonnage",
-            "Late Reporting Tonnage",
-            "Producer Reported Household Tonnage + Late Reporting Tonnage",
-            "Comms Cost - by Material Price Per Tonne",
-        ];
-
-        public required IEnumerable<CalcResultCommsCostRecord> Records { get; init; }
-
-        public required IEnumerable<string> CountryNames { get; init; }
-
-        public override string ToString()
-        {
-            var builder = new StringBuilder();
-
-            // Append the column headers to the report.
-            builder.AppendJoin(Delimiter, Headers1);
-            builder.Append(Delimiter);
-            builder.AppendJoin(Delimiter, CountryNames);
-            builder.Append(Delimiter);
-            builder.AppendJoin(Delimiter, Headers2);
-            builder.Append(Environment.NewLine);
-
-            // Append the records to the report.
-            builder.AppendJoin(Environment.NewLine, Records);
-            return builder.ToString();
-        }
+        public IEnumerable<CalcResultCommsCostOnePlusFourApportionment> CalcResultCommsCostOnePlusFourApportionment { get; set; }
+        public IEnumerable<CalcResultCommsCostCommsCostByMaterial> CalcResultCommsCostCommsCostByMaterial { get; set; }
+        public IEnumerable<CalcResultCommsCostOnePlusFourApportionment> CommsCostByCountry { get; set; }
     }
 }
--- src/EPR.Calculator.API/Builder/CommsCost/CalcResultCommsCostBuilder.cs
+++ src/EPR.Calculator.API/Builder/CommsCost/CalcResultCommsCostBuilder.cs
@@ -1,106 +1,99 @@
-﻿namespace EPR.Calculator.API.Builder.CommsCost
-{
-    using System.Globalization;
-    using System.Text;
-    using EPR.Calculator.API.Data;
-    using EPR.Calculator.API.Data.DataModels;
+﻿using EPR.Calculator.API.Data.DataModels;
 
-    /// <summary>
-    /// Generates the CommsCost report.
-    /// </summary>
-    /// <param name="DBContext">The database context.</param>
-    public class CalcResultCommsCostBuilder(ApplicationDBContext DBContext) : ICalcResultCommsCostBuilder
+namespace EPR.Calculator.API.Builder.CommsCost
+{
+    using EPR.Calculator.API.Data;
+    using EPR.Calculator.API.Models;
+
+    public class CalcResultCommsCostBuilder(ApplicationDBContext context) : ICalcResultCommsCostBuilder
     {
         /// <summary>
         /// The key used to identify household records in the producer_reported_material table.
         /// </summary>
         private const string HouseHoldIndicator = "HH";
 
-
-
-        /// <summary>
-        /// Generate the CommsCost report.
-        /// </summary>
-        /// <param name="totalValues">The total values, indexed by material ID.</param>
-        /// <param name="lateReportingTonnage">
-        /// The late reporting tonnage values, indexed by material ID.
-        /// </param>
-        /// <returns></returns>
-        public CalcResultCommsCost Construct(int runId)
+        public CalcResultCommsCost Construct(int runId, CalcResultOnePlusFourApportionment apportionment)
         {
-            var countries = GetCountryDetails(runId);
-            var materials = GetMaterialDetails(runId);
-            var records = materials.Select(material => new CalcResultCommsCostRecord(material, countries));
+            var apportionmentDetails = apportionment.CalcResultOnePlusFourApportionmentDetails;
+            var apportionmentDetail = apportionmentDetails.Last();
 
-            var report = new CalcResultCommsCost
+            var result = new CalcResultCommsCost();
+            CalculateApportionment(apportionmentDetail, result);
+
+            var materialNames = context.Material.Select(x => x.Name).ToList();
+
+            var allDefaultResults =
+                (from run in context.CalculatorRuns
+                    join defaultMaster in context.DefaultParameterSettings on run.DefaultParameterSettingMasterId equals
+                        defaultMaster.Id
+                    join defaultDetail in context.DefaultParameterSettingDetail on defaultMaster.Id equals defaultDetail
+                        .DefaultParameterSettingMasterId
+                    join defaultTemplate in context.DefaultParameterTemplateMasterList on defaultDetail
+                        .ParameterUniqueReferenceId equals defaultTemplate.ParameterUniqueReferenceId
+                    where run.Id == runId
+                    select new
+                    {
+                        defaultDetail.ParameterValue,
+                        defaultTemplate.ParameterType,
+                        defaultTemplate.ParameterCategory
+                    });
+            var materialDefaults = allDefaultResults.Where(x =>
+                x.ParameterType == "Communication costs by material" && materialNames.Contains(x.ParameterCategory));
+
+            var list = new List<CalcResultCommsCostCommsCostByMaterial>();
+
+            foreach (var materialDefault in materialDefaults)
             {
-                Records = records,
-                CountryNames = countries.Select(c => c.Name)
+                var commsCost = new CalcResultCommsCostCommsCostByMaterial
+                {
+                    EnglandValue = apportionmentDetail.EnglandTotal * materialDefault.ParameterValue,
+                    WalesValue = apportionmentDetail.WalesTotal * materialDefault.ParameterValue,
+                    NorthernIrelandValue = apportionmentDetail.NorthernIrelandTotal * materialDefault.ParameterValue,
+                    ScotlandValue = apportionmentDetail.ScotlandTotal * materialDefault.ParameterValue,
+                    Name = materialDefault.ParameterCategory,
+                    TotalValue = apportionmentDetail.AllTotal * materialDefault.ParameterValue
+                };
+                list.Add(commsCost);
+            }
+
+            new CalcResultCommsCostCommsCostByMaterial
+            {
+                EnglandValue = list.Sum(x => x.EnglandValue),
+                WalesValue = list.Sum(x => x.WalesValue),
+                NorthernIrelandValue = list.Sum(x => x.NorthernIrelandValue),
+                ScotlandValue = list.Sum(x => x.ScotlandValue),
+                TotalValue = list.Sum(x => x.TotalValue),
+            };
+            result.CalcResultCommsCostCommsCostByMaterial = list;
+
+            return result;
+        }
+
+        private static void CalculateApportionment(CalcResultOnePlusFourApportionmentDetail apportionmentDetail,
+            CalcResultCommsCost result)
+        {
+            var commsApportionmentHeader = new CalcResultCommsCostOnePlusFourApportionment
+            {
+                England = "England",
+                Wales = "Wales",
+                Scotland = "Scotland",
+                NorthernIreland = "Northern Ireland",
             };
 
-            return report;
+            var commsApportionments = new List<CalcResultCommsCostOnePlusFourApportionment>();
+            commsApportionments.Add(commsApportionmentHeader);
+
+            var commsApportionment = new CalcResultCommsCostOnePlusFourApportionment
+            {
+                England = apportionmentDetail.EnglandDisposalTotal,
+                Wales = apportionmentDetail.WalesDisposalTotal,
+                Scotland = apportionmentDetail.ScotlandDisposalTotal,
+                NorthernIreland = apportionmentDetail.NorthernIrelandDisposalTotal
+            };
+            commsApportionments.Add(commsApportionment);
+
+            result.CalcResultCommsCostOnePlusFourApportionment = commsApportionments;
         }
 
-        /// <summary>
-        /// Retrieve the materials data from the database.
-        /// </summary>
-        private IEnumerable<MaterialDetails> GetMaterialDetails(int runId)
-        {
-            var masterParameterId = DBContext.CalculatorRuns
-                .Single(run => run.Id == runId)
-                .DefaultParameterSettingMaster?.Id
-                ?? throw new InvalidOperationException("No master parameter record found.");
-            var parameters = DBContext.DefaultParameterSettingDetail
-               .Where(p => p.DefaultParameterSettingMasterId == masterParameterId);
-            if (!parameters.Any())
-            {
-                throw new InvalidOperationException("No parameter details found.");
-            }
-
-
-            // Select the materials details from the database.
-            try
-            {
-                return DBContext.Material.ToList()
-                    .Select((material, parameter) => new MaterialDetails
-                    {
-                        Id = material.Id,
-                        Name = material.Name,
-                        TotalValue = parameters.Single(
-                            p => p.ParameterUniqueReferenceId == $"COMC-{material.Code}").ParameterValue,
-                        LateReportingTonnage = parameters.Single(
-                            p => p.ParameterUniqueReferenceId == $"LRET-{material.Code}").ParameterValue,
-                        ProdRepHoPaWaT = CalculateProdRepHoPaWaT(material)
-                    });
-            }
-            catch (Exception ex)
-            {
-                throw new InvalidOperationException("Could not retrieve material data, see inner exception.", ex);
-            }
-        }
-
-        /// <summary>
-        /// Retrieve the country details from the database.
-        /// </summary>
-        private IEnumerable<CountryDetails> GetCountryDetails(int runId)
-            => from country in DBContext.Country
-               join app in DBContext.CountryApportionment
-               on country.Id equals app.CountryId
-               where app.CalculatorRunId == runId
-               select new CountryDetails
-               {
-                   Id = country.Id,
-                   Name = country.Name,
-                   Apportionment = app.Apportionment,
-               };
-
-        /// <summary>
-        /// Calculates the Producer Reported Household Packaging Waste Tonnage
-        /// by summing the household tonnage reported for the given material by all the producers.
-        /// </summary>
-        private static decimal CalculateProdRepHoPaWaT(Material material)
-            => material.ProducerReportedMaterials
-                .Where(m => m.PackagingType == HouseHoldIndicator)
-                .Sum(m => m.PackagingTonnage);
     }
 }
