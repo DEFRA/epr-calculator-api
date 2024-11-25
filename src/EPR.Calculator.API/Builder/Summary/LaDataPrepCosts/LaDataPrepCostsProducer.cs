99ae4d14600c797c6c687300cdfef12e625d9aaa
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-11-25 21:45:45 +0000

    464059 Total bill 4

---
+++ src/EPR.Calculator.API/Builder/Summary/LaDataPrepCosts/LaDataPrepCostsProducer.cs
@@ -0,0 +1,91 @@
+﻿using EPR.Calculator.API.Models;
+
+namespace EPR.Calculator.API.Builder.Summary.LaDataPrepCosts
+{
+    public class LaDataPrepCostsProducer
+    {
+        private static readonly int columnIndex = 223;
+
+        public static IEnumerable<CalcResultSummaryHeader> GetHeaders()
+        {
+            return [
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.TotalProducerFeeWithoutBadDebtProvision , ColumnIndex = columnIndex },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.BadDebtProvision, ColumnIndex = columnIndex + 1 },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.TotalProducerFeeWithBadDebtProvision, ColumnIndex = columnIndex + 2 },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.EnglandTotalWithBadDebtProvision, ColumnIndex = columnIndex + 3 },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.WalesTotalWithBadDebtProvision, ColumnIndex = columnIndex + 4 },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.ScotlandTotalWithBadDebtProvision, ColumnIndex = columnIndex + 5 },
+                new CalcResultSummaryHeader { Name = LaDataPrepCostsHeaders.NorthernIrelandTotalWithBadDebtProvision, ColumnIndex = columnIndex + 6 }
+            ];
+        }
+
+        public static decimal GetBadDebtProvision()
+        {
+            return 112;
+        }
+        public static decimal GetBadDebtProvisionTotal()
+        {
+            return 112;
+        }
+
+        public static decimal GetProducerFeeWithoutBadDebtProvision()
+        {
+            return 113;
+        }
+
+        public static decimal GetProducerFeeWithoutBadDebtProvisionTotal()
+        {
+            return 113;
+        }
+
+        public static decimal GetProducerFeeWithBadDebtProvision()
+        {
+            return 114;
+        }
+
+        public static decimal GetProducerFeeWithBadDebtProvisionTotal()
+        {
+            return 115;
+        }
+
+        public static decimal GetEnglandTotalWithBadDebtProvision()
+        {
+            return 116;
+        }
+
+        public static decimal GetEnglandOverallTotalWithBadDebtProvision()
+        {
+            return 116;
+        }
+
+        public static decimal GetWalesTotalWithBadDebtProvision()
+        {
+            return 116;
+        }
+
+        public static decimal GetWalesOverallTotalWithBadDebtProvision()
+        {
+            return 116;
+        }
+
+        public static decimal GetScotlandTotalWithBadDebtProvision()
+        {
+            return 117;
+        }
+
+        public static decimal GetScotlandOverallTotalWithBadDebtProvision()
+        {
+            return 117;
+        }
+
+        public static decimal GetNorthernIrelandTotalWithBadDebtProvision()
+        {
+            return 118;
+        }
+
+        public static decimal GetNorthernIrelandOverallTotalWithBadDebtProvision()
+        {
+            return 118;
+        }
+    }
+}
---
+++ src/EPR.Calculator.API/Builder/Summary/LaDataPrepCosts/LaDataPrepCostsSummary.cs
@@ -0,0 +1,33 @@
+﻿using EPR.Calculator.API.Models;
+
+namespace EPR.Calculator.API.Builder.Summary.LaDataPrepCosts
+{
+    public class LaDataPrepCostsSummary
+    {
+        private static readonly int columnIndex = 216;
+
+        public static IEnumerable<CalcResultSummaryHeader> GetHeaders()
+        {
+            return [
+                new CalcResultSummaryHeader { Name = $"{LaDataPrepCostsHeaders.LaDataPrepCostsWithoutBadDebtProvisionTitle}", ColumnIndex = columnIndex },
+                new CalcResultSummaryHeader { Name = $"{LaDataPrepCostsHeaders.BadDebtProvisionTitle}", ColumnIndex = columnIndex + 1 },
+                new CalcResultSummaryHeader { Name = $"{LaDataPrepCostsHeaders.LaDataPrepCostsWithBadDebtProvisionTitle}", ColumnIndex = columnIndex + 2 }
+            ];
+        }
+
+        public static decimal GetBadDebtProvision()
+        {
+            return 109;
+        }
+
+        public static decimal GetLaDataPrepCostsWithoutBadDebtProvision()
+        {
+            return 110;
+        }
+
+        public static decimal GetLaDataPrepCostsWithBadDebtProvision()
+        {
+            return 111;
+        }
+    }
+}
