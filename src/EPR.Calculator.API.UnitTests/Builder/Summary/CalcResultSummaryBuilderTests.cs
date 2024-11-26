0ea25b2f0de301e5fbc8e4ec5a1a43390e4d092d
udaydefra <uday.2.denduluri.external@eviden.com>
2024-11-26 13:34:24 +0000

    refactoring

--- src/EPR.Calculator.API.UnitTests/Builder/Summary/CalcResultSummaryBuilderTests.cs
+++ src/EPR.Calculator.API.UnitTests/Builder/Summary/CalcResultSummaryBuilderTests.cs
@@ -1,4 +1,5 @@
 ﻿using EPR.Calculator.API.Builder.Summary;
+using EPR.Calculator.API.Builder.Summary.OneAndTwoA;
 using EPR.Calculator.API.Constants;
 using EPR.Calculator.API.Data;
 using EPR.Calculator.API.Data.DataModels;
@@ -353,7 +354,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.BadDebtProvisionFor1 = 100m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor1);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor1);
 
             Assert.AreEqual(100m, totalFee);
         }
@@ -370,7 +371,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.TotalProducerDisposalFeeWithBadDebtProvision = 200m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerDisposalFeeWithBadDebtProvision);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerDisposalFeeWithBadDebtProvision);
 
             Assert.AreEqual(200m, totalFee);
         }
@@ -387,7 +388,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.TotalProducerCommsFee = 300m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerCommsFee);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerCommsFee);
 
             Assert.AreEqual(300m, totalFee);
         }
@@ -404,7 +405,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.BadDebtProvisionFor2A = 400m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor2A);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor2A);
 
             Assert.AreEqual(400m, totalFee);
         }
@@ -421,7 +422,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.TotalProducerCommsFeeWithBadDebtProvision = 500m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerCommsFeeWithBadDebtProvision);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.TotalProducerCommsFeeWithBadDebtProvision);
 
             Assert.AreEqual(500m, totalFee);
         }
@@ -438,7 +439,7 @@ namespace EPR.Calculator.API.UnitTests
             totalRow.BadDebtProvisionFor1 = 0m;
             totalRow.Level = "Totals";
 
-            var totalFee = CalcResultSummaryBuilder.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor1);
+            var totalFee = CalcResultOneAndTwoAUtil.GetTotalFee(result.ProducerDisposalFees.ToList(), fee => fee.BadDebtProvisionFor1);
 
             Assert.AreEqual(0m, totalFee);
         }
@@ -446,7 +447,7 @@ namespace EPR.Calculator.API.UnitTests
         [TestMethod]
         public void GetTotalFee_ShouldReturnZero_WhenFeesIsNull()
         {
-            var result = CalcResultSummaryBuilder.GetTotalFee(null, fee => fee.BadDebtProvisionFor1);
+            var result = CalcResultOneAndTwoAUtil.GetTotalFee(null, fee => fee.BadDebtProvisionFor1);
 
             Assert.AreEqual(0m, result);
         }
