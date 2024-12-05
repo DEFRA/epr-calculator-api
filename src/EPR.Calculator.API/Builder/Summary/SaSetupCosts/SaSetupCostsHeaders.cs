e322ce7ce09387d63874672962d05e5ec8399172
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-12-05 09:15:30 +0000

    Update SaSetupCostsHeaders.cs

--- src/EPR.Calculator.API/Builder/Summary/SaSetupCosts/SaSetupCostsHeaders.cs
+++ src/EPR.Calculator.API/Builder/Summary/SaSetupCosts/SaSetupCostsHeaders.cs
@@ -6,7 +6,7 @@
         public static readonly string OneOffFeeSetupCostsWithBadDebtProvisionTitle = "5 One-off fee for SA Set Up Costs with Bad Debt provision";
         public static readonly string OneOffFeeSetupCostsWithoutBadDebtProvisionTitle = "5 One-off fee for SA Set Up Costs w/o Bad Debt provision";
 
-        public static readonly string BadDebtProvision = "Bad Debt Provision for 4";
+        public static readonly string BadDebtProvision = "Bad Debt Provision for 5";
         public static readonly string ProducerOneOffFeeWithBadDebtProvision = "5 Total Producer One-off fee for SA Set Up Costs In proportion to (1+2a) with Bad Debt provision";
         public static readonly string ProducerOneOffFeeWithoutBadDebtProvision = "5 Total Producer One-off fee for SA Set Up Costs In proportion to (1+2a) w/o Bad Debt provision";
         public static readonly string EnglandTotalWithBadDebtProvision = "England Total with Bad Debt provision";
