fc23a2da60bc5940b9199cecee7bdac1becc6082
johnbenjamin-defra <john.2.benjamin.external@eviden.com>
2024-12-17 14:26:42 +0000

    Update OnePlus2A2B2CHeaders.cs

--- src/EPR.Calculator.API/Builder/Summary/OnePlus2A2B2C/OnePlus2A2B2CHeaders.cs
+++ src/EPR.Calculator.API/Builder/Summary/OnePlus2A2B2C/OnePlus2A2B2CHeaders.cs
@@ -1,9 +1,9 @@
 ﻿namespace EPR.Calculator.API.Builder.Summary.OnePlus2A2B2C
 {
-    public static class OnePlus2A2B2CHeaders
+    public class OnePlus2A2B2CHeaders
     {
-        public static readonly string TotalWithBadDebtProvision = "Total (1+2a+2b+2c) with Bad Debt provision";
-        public static readonly string ProducerTotalWithBadDebtProvision = "Producer Total (1+2a+2b+2c) with Bad Debt provision";
-        public static readonly string ProducerPercentageOfOverallProducerCost = "Producer Percentage of Overall Producer Cost for (1+2a+2b+2c)";
+        public const string TotalWithBadDebtProvision = "Total (1+2a+2b+2c) with Bad Debt provision";
+        public const string ProducerTotalWithBadDebtProvision = "Producer Total (1+2a+2b+2c) with Bad Debt provision";
+        public const string ProducerPercentageOfOverallProducerCost = "Producer Percentage of Overall Producer Cost for (1+2a+2b+2c)";
     }
 }
