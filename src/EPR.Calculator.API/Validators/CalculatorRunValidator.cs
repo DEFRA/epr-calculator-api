950bf4801672f962d870bad9eac53bdb4c7d8ce0
Rekha Devi <rekha.devi.external@eviden.com>
2025-01-21 09:57:03 +0000

    Adding 422 error handling implementation

--- src/EPR.Calculator.API/Validators/CalculatorRunValidator.cs
+++ src/EPR.Calculator.API/Validators/CalculatorRunValidator.cs
@@ -1,4 +1,5 @@
 ﻿using EPR.Calculator.API.Constants;
+using EPR.Calculator.API.Data.DataModels;
 using FluentValidation;
 
 namespace EPR.Calculator.API.Validators
@@ -8,6 +9,28 @@ namespace EPR.Calculator.API.Validators
         public CalculatorRunValidator()
         {
             RuleFor(x => x).NotEmpty().WithMessage("Calculator Run Name is Required");
+        }
+        public ValidationResult ValidateCalculatorRunIds(CalculatorRun calculatorRun)
+        {
+            var errorMessages = new List<string>();
+
+            var requiredMasterIds = new Dictionary<string, int?>
+        {
+            { "CalculatorRunOrganisationDataMasterId", calculatorRun.CalculatorRunOrganisationDataMasterId },
+            { "DefaultParameterSettingMasterId", calculatorRun.DefaultParameterSettingMasterId },
+            { "CalculatorRunPomDataMasterId", calculatorRun.CalculatorRunPomDataMasterId },
+            { "LapcapDataMasterId", calculatorRun.LapcapDataMasterId }
+        };
+
+            errorMessages.AddRange(requiredMasterIds
+                .Where(id => id.Value == null)
+                .Select(id => $"{id.Key} is null"));
+
+            return new ValidationResult
+            {
+                IsValid = errorMessages.Count == 0,
+                ErrorMessages = errorMessages
+            };
         }
     }
 }
