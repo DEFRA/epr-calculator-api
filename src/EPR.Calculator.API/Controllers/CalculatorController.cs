cd8742a780258e84221096dba4ceefe94592df61
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-01-22 15:49:34 +0000

    removed commented line

--- src/EPR.Calculator.API/Controllers/CalculatorController.cs
+++ src/EPR.Calculator.API/Controllers/CalculatorController.cs
@@ -119,7 +119,7 @@ namespace EPR.Calculator.API.Controllers
                         };
 
                         // Send message
-                        // await SendMessage(serviceBusQueueName, calculatorRunMessage);
+                        await SendMessage(serviceBusQueueName, calculatorRunMessage);
 
                         // All good, commit transaction
                         await transaction.CommitAsync();
