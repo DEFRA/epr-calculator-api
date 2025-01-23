4415ba40ccebcbc8b6c1f73059ff6c89b922b421
rekhadevieviden <rekha.devi.external@eviden.com>
2025-01-23 09:52:25 +0000

    Adding 422 error handling implementation RD (#177)
    * Adding 422 error handling implementation
    
    * alignment comment fixes
    
    * Resolve the comment to send an array as result

---
+++ src/EPR.Calculator.API/Validators/ValidationResult.cs
@@ -0,0 +1,12 @@
+﻿namespace EPR.Calculator.API.Validators
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public IEnumerable<string> ErrorMessages { get; set; }
+        public ValidationResult()
+        {
+            ErrorMessages = new List<string>();
+        }
+    }
+}
