df21cdf4288e499524104449cc8d19205c43f625
udaydefra <uday.2.denduluri.external@eviden.com>
2025-04-11 11:55:13 +0100

    new Migrations

---
+++ src/EPR.Calculator.API.Data/Migrations/20250411104742_AddNewRunClassifications.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewRunClassifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+               table: "calculator_run_classification",
+               columns: new[]
+               {
+                    "status",
+                    "created_by",
+               },
+               values: new object[,]
+               {
+                    {
+                        "INITIAL RUN",
+                        "Test user",
+                    },
+                    {
+                        "INTERIM RE-CALCULATION RUN",
+                        "Test user",
+                    },
+                    {
+                        "FINAL RUN",
+                        "Test user",
+                    },
+                    {
+                        "FINAL RE-CALCULATION RUN",
+                        "Test user",
+                    },
+               });
+
+            migrationBuilder.UpdateData(
+                table: "calculator_run_classification",
+                keyColumn: "id",
+                keyValue: 4,
+                column: "status",
+                value: "TEST RUN"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "calculator_run_classification",
+                keyColumn: "status",
+                keyValue: "INITIAL RUN");
+
+            migrationBuilder.DeleteData(
+                table: "calculator_run_classification",
+                keyColumn: "status",
+                keyValue: "INTERIM RE-CALCULATION RUN");
+
+            migrationBuilder.DeleteData(
+                table: "calculator_run_classification",
+                keyColumn: "status",
+                keyValue: "FINAL RUN");
+
+            migrationBuilder.DeleteData(
+                table: "calculator_run_classification",
+                keyColumn: "status",
+                keyValue: "FINAL RE-CALCULATION RUN");
+            migrationBuilder.UpdateData(
+                table: "calculator_run_classification",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "status",
+                value: "PLAY"
+            );
+        }
+    }
+}
