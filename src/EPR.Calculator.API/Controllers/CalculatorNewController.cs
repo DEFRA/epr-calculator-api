00d8828a28b94f9050e40e09900e18c4e65f34a3
udaydefra <uday.2.denduluri.external@eviden.com>
2025-04-28 08:20:56 +0100

    Adding new CalculatorNewController

---
+++ src/EPR.Calculator.API/Controllers/CalculatorNewController.cs
@@ -0,0 +1,19 @@
+﻿using EPR.Calculator.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EPR.Calculator.API.Controllers
+{
+    [Route("V2")]
+    [ApiController]
+    public class CalculatorNewController : ControllerBase
+    {
+        [HttpPut]
+        [Route("calculatorRuns")]
+        [AllowAnonymous]
+        public async Task<IActionResult> PutCalculatorRunStatus(CalculatorRunStatusUpdateDto runStatusUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
