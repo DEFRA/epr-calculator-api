86a48fd3bb16de005952e9a530468d6d1cb23a7c
Uday Denduluri <uday.2.denduluri.external@eviden.com>
2025-05-01 13:08:57 +0100

    Feature/526640 put calc run (#228)
    * Adding Migrations - UD
    
    * Adding new CalculatorNewController
    
    * Adding validator
    
    * Adding Generic validation
    
    * Added Validator
    
    * Adding validator
    
    * Adding Validator
    
    * Adding unit test
    
    * Add more Unit test coverage
    
    * Fixed unit tests

---
+++ src/EPR.Calculator.API/Controllers/CalculatorNewController.cs
@@ -0,0 +1,83 @@
+﻿using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Dtos;
+using EPR.Calculator.API.Validators;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EPR.Calculator.API.Controllers
+{
+    [Route("V2")]
+    [ApiController]
+    public class CalculatorNewController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly ICalculatorRunStatusDataValidator calculatorRunStatusDataValidator;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CalculatorNewController"/> class.
+        /// </summary>
+        /// <param name="context">Db Context</param>
+        /// <param name="calculatorRunStatusDataValidator">Db Validator</param>
+        public CalculatorNewController(
+            ApplicationDBContext context,
+            ICalculatorRunStatusDataValidator calculatorRunStatusDataValidator)
+        {
+            this.context = context;
+            this.calculatorRunStatusDataValidator = calculatorRunStatusDataValidator;
+        }
+
+        [HttpPut]
+        [Route("calculatorRuns")]
+        public async Task<IActionResult> PutCalculatorRunStatus(CalculatorRunStatusUpdateDto runStatusUpdateDto)
+        {
+            try
+            {
+                var claim = this.User.Claims.FirstOrDefault(x => x.Type == "name");
+                if (claim == null)
+                {
+                    return new ObjectResult("No claims in the request") { StatusCode = StatusCodes.Status401Unauthorized };
+                }
+
+                var userName = claim.Value;
+
+                var classification = await this.context.CalculatorRunClassifications.SingleOrDefaultAsync(x =>
+                    x.Id == runStatusUpdateDto.ClassificationId);
+
+                if (classification == null)
+                {
+                    return new ObjectResult($"Unable to find Classification Id {runStatusUpdateDto.ClassificationId}")
+                    { StatusCode = StatusCodes.Status422UnprocessableEntity };
+                }
+
+                var calculatorRun = await this.context.CalculatorRuns.SingleOrDefaultAsync(
+                            x => x.Id == runStatusUpdateDto.RunId);
+                if (calculatorRun == null)
+                {
+                    return new ObjectResult($"Unable to find Run Id {runStatusUpdateDto.RunId}")
+                    { StatusCode = StatusCodes.Status422UnprocessableEntity };
+                }
+
+                var validationResult = this.calculatorRunStatusDataValidator.Validate(calculatorRun, runStatusUpdateDto);
+
+                if (validationResult.IsInvalid)
+                {
+                    return new ObjectResult(validationResult.Errors)
+                    { StatusCode = StatusCodes.Status422UnprocessableEntity };
+                }
+
+                calculatorRun.CalculatorRunClassificationId = runStatusUpdateDto.ClassificationId;
+                calculatorRun.UpdatedAt = DateTime.UtcNow;
+                calculatorRun.UpdatedBy = userName;
+
+                this.context.CalculatorRuns.Update(calculatorRun);
+                await this.context.SaveChangesAsync();
+
+                return this.StatusCode(201);
+            }
+            catch (Exception exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, exception);
+            }
+        }
+    }
+}
---
+++ src/EPR.Calculator.API/Dtos/GenericValidationResultDto.cs
@@ -0,0 +1,14 @@
+﻿namespace EPR.Calculator.API.Dtos
+{
+    public class GenericValidationResultDto
+    {
+        public GenericValidationResultDto()
+        {
+            this.Errors = new List<string>();
+        }
+
+        public List<string> Errors { get; set; }
+
+        public bool IsInvalid { get; set; }
+    }
+}
