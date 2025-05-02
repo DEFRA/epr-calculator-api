7719cad5411ae6d1c9ae5241986d9d9495288ed5
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-05-02 15:17:07 +0100

    Feature/543370 integrate calc run details (#231)
    * Updated API for createdBy
    
    * update mapper
    
    * fix unit test
    
    * refactored code

--- src/EPR.Calculator.API/Dtos/CalculatorRunDto.cs
+++ src/EPR.Calculator.API/Dtos/CalculatorRunDto.cs
@@ -10,12 +10,16 @@
 
         public required string FileExtension { get; set; }
 
-        public DateTime? UpdatedBy { get; set; }
+        public string? UpdatedBy { get; set; }
+
+        public string CreatedBy { get; set; }
 
         public DateTime? UpdatedAt { get; set; }
 
         public int RunClassificationId { get; set; }
 
         public required string RunClassificationStatus { get; set; }
+
+        public string FinancialYear { get; set; }
     }
 }
