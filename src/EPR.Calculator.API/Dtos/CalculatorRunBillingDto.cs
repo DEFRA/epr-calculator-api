1dad0f5ee462749082c6cbe4ede1829fca0916bf
Mazar Shaik <mazar.shaik.external@eviden.com>
2025-05-15 15:23:21 +0100

    changed to record from class as per review comment

--- src/EPR.Calculator.API/Dtos/CalculatorRunBillingDto.cs
+++ src/EPR.Calculator.API/Dtos/CalculatorRunBillingDto.cs
@@ -2,40 +2,40 @@
 
 namespace EPR.Calculator.API.Dtos
 {
-    public class CalculatorRunBillingDto
+    public record CalculatorRunBillingDto
     {
-        public int RunId { get; set; }
+        public int RunId { get; init; }
 
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; init; }
 
-        public required string RunName { get; set; }
+        public required string RunName { get; init; }
 
-        public required string FileExtension { get; set; }
+        public required string FileExtension { get; init; }
 
-        public string? UpdatedBy { get; set; }
+        public string? UpdatedBy { get; init; }
 
-        public string CreatedBy { get; set; }
+        public string CreatedBy { get; init; }
 
-        public DateTime? UpdatedAt { get; set; }
+        public DateTime? UpdatedAt { get; init; }
 
-        public int RunClassificationId { get; set; }
+        public int RunClassificationId { get; init; }
 
-        public required string RunClassificationStatus { get; set; }
+        public required string RunClassificationStatus { get; init; }
 
-        public string FinancialYear { get; set; }
+        public string FinancialYear { get; init; }
 
-        public int? BillingFileId { get; set; }
+        public int? BillingFileId { get; init; }
 
-        public string? BillingCsvFileName { get; set; }
+        public string? BillingCsvFileName { get; init; }
 
-        public string? BillingJsonFileName { get; set; }
+        public string? BillingJsonFileName { get; init; }
 
-        public DateTime? BillingFileCreatedDate { get; set; }
+        public DateTime? BillingFileCreatedDate { get; init; }
 
-        public string? BillingFileCreatedBy { get; set; }
+        public string? BillingFileCreatedBy { get; init; }
 
-        public DateTime? BillingFileAuthorisedDate { get; set; }
+        public DateTime? BillingFileAuthorisedDate { get; init; }
 
-        public string? BillingFileAuthorisedBy { get; set; }
+        public string? BillingFileAuthorisedBy { get; init; }
     }
 }
