6436e7b485784b61d327b51519ea03c8cb60ef0e
Shilpi Goel <Shilpi.Goel@goaco.com>
2025-07-17 16:58:07 +0100

    Historical year view - Future Years

---
+++ src/EPR.Calculator.API.Data/Migrations/20250717154431_DeleteFinancialYearsData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteFinancialYearsData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+               table: "calculator_run_financial_years",
+               keyColumn: "financial_Year",
+               keyValue: "2026-27");
+
+            migrationBuilder.DeleteData(
+               table: "calculator_run_financial_years",
+               keyColumn: "financial_Year",
+               keyValue: "2027-28");
+
+            migrationBuilder.DeleteData(
+               table: "calculator_run_financial_years",
+               keyColumn: "financial_Year",
+               keyValue: "2028-29");
+
+            migrationBuilder.DeleteData(
+               table: "calculator_run_financial_years",
+               keyColumn: "financial_Year",
+               keyValue: "2029-30");
+
+            migrationBuilder.DeleteData(
+               table: "calculator_run_financial_years",
+               keyColumn: "financial_Year",
+               keyValue: "2030-31");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "calculator_run_financial_years",
+                columns: new[] { "financial_Year", "description" },
+                values: new object[,]
+                {
+                    { "2026-27", null },
+                    { "2026-27", null },
+                    { "2026-27", null },
+                    { "2026-27", null },
+                    { "2026-27", null },
+                });
+        }
+    }
+}
