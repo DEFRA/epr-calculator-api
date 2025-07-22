8bf9be297cc614125e5b197444166e13bc1ad406
Rekha Devi <rekha.devi.external@atos.net>
2025-07-22 19:03:53 +0100

    Critical iisues fixes on sonar

--- src/EPR.Calculator.API.Data/Migrations/SQLScripts/20250721090348_AddNewPostInitialStatuses_ReleaseScript.sql
+++ src/EPR.Calculator.API.Data/Migrations/SQLScripts/20250721090348_AddNewPostInitialStatuses_ReleaseScript.sql
@@ -2,6 +2,10 @@
 GO
 
 DECLARE @var0 sysname;
+
+DECLARE @SystemUser NVARCHAR(100) = N'System User';
+DECLARE @TestUser NVARCHAR(100) = N'Test User';
+
 SELECT @var0 = [d].[name]
 FROM [sys].[default_constraints] [d]
 INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
@@ -13,15 +17,15 @@ GO
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'id', N'created_by', N'status') AND [object_id] = OBJECT_ID(N'[calculator_run_classification]'))
     SET IDENTITY_INSERT [calculator_run_classification] ON;
 INSERT INTO [calculator_run_classification] ([id], [created_by], [status])
-VALUES (6, N'System User', N'DELETED'),
-(7, N'System User', N'INITIAL RUN COMPLETED'),
-(8, N'Test User', N'INITIAL RUN'),
-(9, N'Test User', N'INTERIM RE-CALCULATION RUN'),
-(10, N'Test User', N'FINAL RUN'),
-(11, N'Test User', N'FINAL RE-CALCULATION RUN'),
-(12, N'System User', N'INTERIM RE-CALCULATION RUN COMPLETED'),
-(13, N'System User', N'FINAL RE-CALCULATION RUN COMPLETED'),
-(14, N'System User', N'FINAL RUN COMPLETED');
+VALUES (6, @SystemUser, N'DELETED'),
+(7, @SystemUser, N'INITIAL RUN COMPLETED'),
+(8, @TestUser, N'INITIAL RUN'),
+(9, @TestUser, N'INTERIM RE-CALCULATION RUN'),
+(10, @TestUser, N'FINAL RUN'),
+(11, @TestUser, N'FINAL RE-CALCULATION RUN'),
+(12, @SystemUser, N'INTERIM RE-CALCULATION RUN COMPLETED'),
+(13, @SystemUser, N'FINAL RE-CALCULATION RUN COMPLETED'),
+(14, @SystemUser, N'FINAL RUN COMPLETED');
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'id', N'created_by', N'status') AND [object_id] = OBJECT_ID(N'[calculator_run_classification]'))
     SET IDENTITY_INSERT [calculator_run_classification] OFF;
 GO
