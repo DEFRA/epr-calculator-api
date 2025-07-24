a0ac4c44f85b16b9335f955059039fb77a4363c2
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-07-24 10:44:46 +0100

    update the column precision (#287)

---
+++ src/EPR.Calculator.API.Data/Migrations/20250723132540_UpdateInvoiceNetTonnageColumnPrecision.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateInvoiceNetTonnageColumnPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "invoiced_net_tonnage",
+                table: "producer_invoiced_material_net_tonnage",
+                type: "decimal(18,3)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "invoiced_net_tonnage",
+                table: "producer_invoiced_material_net_tonnage",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+    }
+}
