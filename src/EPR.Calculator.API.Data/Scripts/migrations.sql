3f3a4d5637b0388706ea0179c4472d644d2e0ec6
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-07-24 17:47:10 +0100

    Add migration sql

--- src/EPR.Calculator.API.Data/Scripts/migrations.sql
+++ src/EPR.Calculator.API.Data/Scripts/migrations.sql
@@ -4398,3 +4398,34 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250724153138_UpdateNetTonnagePrecision'
+)
+BEGIN
+    DECLARE @var30 sysname;
+    SELECT @var30 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[producer_invoiced_material_net_tonnage]') AND [c].[name] = N'invoiced_net_tonnage');
+    IF @var30 IS NOT NULL EXEC(N'ALTER TABLE [producer_invoiced_material_net_tonnage] DROP CONSTRAINT [' + @var30 + '];');
+    ALTER TABLE [producer_invoiced_material_net_tonnage] ALTER COLUMN [invoiced_net_tonnage] decimal(18,3) NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20250724153138_UpdateNetTonnagePrecision'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20250724153138_UpdateNetTonnagePrecision', N'8.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
