989945b60ee49c4b915a34ba04180ce91a8d0c94
Uday Denduluri <uday.2.denduluri.external@eviden.com>
2025-07-25 14:31:52 +0100

    Alter producedure

---
+++ src/EPR.Calculator.API.Data/Migrations/20250725133129_AlterInsertInvoiceDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterInsertInvoiceDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
