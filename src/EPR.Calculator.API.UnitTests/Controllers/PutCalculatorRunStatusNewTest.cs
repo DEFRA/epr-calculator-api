b184734453b2e0f0b5e6b082ce9bbc5830aa1347
Lee Carswell (ext) <lee.carswell.external@atos.net>
2025-07-28 11:42:47 +0100

    Revert "Fixing merge issue"
    This reverts commit c5e1cc51d2b2975ca064f3252c150931a7d754bd.

--- src/EPR.Calculator.API.UnitTests/Controllers/PutCalculatorRunStatusNewTest.cs
+++ src/EPR.Calculator.API.UnitTests/Controllers/PutCalculatorRunStatusNewTest.cs
@@ -6,7 +6,6 @@ using EPR.Calculator.API.Services.Abstractions;
 using EPR.Calculator.API.Validators;
 using EPR.Calculator.API.Wrapper;
 using Microsoft.ApplicationInsights;
-using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,7 +23,8 @@ namespace EPR.Calculator.API.UnitTests.Controllers
         private readonly Mock<ICalculatorRunStatusDataValidator> mockValidator;
         private readonly Mock<IBillingFileService> mockBillingFileService;
         private readonly Mock<IOrgAndPomWrapper> mockOrgAndPomWrapper;
-        
+        private readonly Mock<TelemetryClient> mockTelemetryClient;
+
         private ApplicationDBContext context;
         private CalculatorNewController controller;
 
@@ -41,15 +41,14 @@ namespace EPR.Calculator.API.UnitTests.Controllers
             this.mockValidator = new Mock<ICalculatorRunStatusDataValidator>();
             this.mockBillingFileService = new Mock<IBillingFileService>();
             this.mockOrgAndPomWrapper = new Mock<IOrgAndPomWrapper>();
-            var config = TelemetryConfiguration.CreateDefault();
-            var telemetryClient = new TelemetryClient(config); ;
+            this.mockTelemetryClient = new Mock<TelemetryClient>();
 
             this.controller = new CalculatorNewController(
                 this.context,
                 this.mockValidator.Object,
                 this.mockBillingFileService.Object,
                 this.mockOrgAndPomWrapper.Object,
-                telemetryClient);
+                this.mockTelemetryClient.Object);
 
             this.context.CalculatorRuns.Add(new CalculatorRun
             {
