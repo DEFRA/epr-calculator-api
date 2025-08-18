d7bef6fa5e8d4e9160f3d113345e32fa54204bc6
David Jayakumar <david.2.jayakumar.external@eviden.com>
2025-08-18 13:25:52 +0100

    Fix david (#305)

---
+++ src/EPR.Calculator.API.Data/Migrations/20250815134048_AddProducerIdIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EPR.Calculator.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProducerIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+              name: "IX_producer_resultfile_suggested_billing_instruction_producer_id",
+              table: "producer_resultfile_suggested_billing_instruction",
+              column: "producer_id");
+
+            migrationBuilder.CreateIndex(
+              name: "IX_producer_detail_producer_id",
+              table: "producer_detail",
+              column: "producer_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+              name: "IX_producer_resultfile_suggested_billing_instruction_producer_id",
+              table: "producer_resultfile_suggested_billing_instruction");
+
+            migrationBuilder.DropIndex(
+              name: "IX_producer_detail_producer_id",
+              table: "producer_detail");
+        }
+    }
+}
