c00971912f43fd79719a41fdc9b4e6f49eef55a5
Santhosh kumar <111443660+Skumarhpa@users.noreply.github.com>
2025-10-07 13:34:38 +0100

    update the function (#324)
    * update the function
    
    * update the cancel condition for accepted

--- src/EPR.Calculator.API.Data/Scripts/migrations.sql
+++ src/EPR.Calculator.API.Data/Scripts/migrations.sql
@@ -5269,3 +5269,67 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003125307_AlterGetCurrentYearInvoicedTotalAfterThisRunFunction'
+)
+BEGIN
+    IF OBJECT_ID(N'[dbo].[GetCurrentYearInvoicedTotalAfterThisRun]', 'FN') IS NOT NULL  
+    DROP FUNCTION [dbo].GetCurrentYearInvoicedTotalAfterThisRun
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003125307_AlterGetCurrentYearInvoicedTotalAfterThisRunFunction'
+)
+BEGIN
+    DECLARE @sql NVARCHAR(MAX) 
+    SET @sql = N'CREATE FUNCTION [dbo].[GetCurrentYearInvoicedTotalAfterThisRun] ( 
+        @billingInstructionAcceptReject      VARCHAR(250),
+        @suggestedBillingInstruction         VARCHAR(250),
+        @currentYearInvoicedTotalToDate      DECIMAL(18,2),
+        @invoiceAmount                       DECIMAL(18,2)
+    )
+    RETURNS DECIMAL(18,2)
+    AS
+    BEGIN
+        -- Rule 1: Cancelled and Rejected instruction always returns last invoiced values
+        IF @suggestedBillingInstruction = ''CANCEL'' AND @billingInstructionAcceptReject = ''Rejected''
+             RETURN ISNULL(@currentYearInvoicedTotalToDate, 0);
+
+        -- Rule 2: Cancelled instruction always returns NULL
+        IF @suggestedBillingInstruction = ''CANCEL'' AND @billingInstructionAcceptReject = ''Accepted''
+            RETURN NULL;
+
+        -- Rule 3: Rejected INITIAL returns NULL
+        IF @billingInstructionAcceptReject = ''Rejected'' AND @suggestedBillingInstruction = ''INITIAL''
+            RETURN NULL;
+
+        -- Rule 4: Rejected (but not INITIAL) returns current total as-is
+        IF @billingInstructionAcceptReject = ''Rejected''
+            RETURN ISNULL(@currentYearInvoicedTotalToDate, 0);
+
+        -- Rule 5: Accepted or any other case adds invoice amount
+        RETURN ISNULL(@currentYearInvoicedTotalToDate, 0) + ISNULL(@invoiceAmount, 0);
+    END'
+    EXEC(@sql)
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20251003125307_AlterGetCurrentYearInvoicedTotalAfterThisRunFunction'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20251003125307_AlterGetCurrentYearInvoicedTotalAfterThisRunFunction', N'8.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
