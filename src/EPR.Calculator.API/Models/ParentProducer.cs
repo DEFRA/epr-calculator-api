d18dedb65f1962d1c21d11aca616a48d15537c51
John Jebastin Benjamin <john.2.benjamin.external@eviden.com>
2025-10-22 20:25:27 +0100

    628239 Cancelled producers display name fix (#328)
    * 628239 Cancelled producers display name fix
    
    * 628239 Cancelled producers display name fix
    
    ---------
    
    Co-authored-by: Uday Denduluri <uday.2.denduluri.external@atos.net>

--- src/EPR.Calculator.API/Models/ParentProducer.cs
+++ src/EPR.Calculator.API/Models/ParentProducer.cs
@@ -2,6 +2,8 @@
 {
     public class ParentProducer
     {
+        public int Id { get; set; }
+
         public int ProducerId { get; set; }
 
         public string ProducerName { get; set; } = string.Empty;
