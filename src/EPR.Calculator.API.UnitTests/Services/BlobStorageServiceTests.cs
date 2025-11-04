7c4a8bed6556e072909e332b04480c886a9dfb6d
Masood Afzal <masood.afzal.external@eviden.com>
2025-11-04 10:53:42 +0000

    feat: Fixed code smells based on Sonar Qube analysis

--- src/EPR.Calculator.API.UnitTests/Services/BlobStorageServiceTests.cs
+++ src/EPR.Calculator.API.UnitTests/Services/BlobStorageServiceTests.cs
@@ -79,7 +79,7 @@ namespace EPR.Calculator.API.UnitTests.Services
                 details: downloadDetails);
 
             this.mockBlobClient.Setup(x => x.ExistsAsync(default)).ReturnsAsync(Response.FromValue(true, null!));
-            this.mockBlobClient.Setup(x => x.DownloadContentAsync(TestContext.CancellationTokenSource.Token)).ReturnsAsync(Response.FromValue(downloadResult, null!));
+            this.mockBlobClient.Setup(x => x.DownloadContentAsync()).ReturnsAsync(Response.FromValue(downloadResult, null!));
             this.mockBlobClient.Setup(x => x.Uri).Returns(new Uri(blobUri));
             blobUri = string.Empty;
 
