220de177175659d6844318d2ff25f0c1693a651c
Ashley Wright <ashley.wright@equalexperts.com>
2026-02-20 16:24:30 +0000

    Ecv 382 (#374)
    * ECV-382 Fix CurrentYearInvoicedTotalAfterThisRun for Rebills
    
    * ECV-382 Add rollback migration for R17.1
    
    ---------
    
    Co-authored-by: colin-lamed <9568290+colin-lamed@users.noreply.github.com>

--- src/EPR.Calculator.API.Data/Scripts/migrations.sql
+++ src/EPR.Calculator.API.Data/Scripts/migrations.sql
@@ -6405,3 +6405,81 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260218155412_UpdateGetCurrentYearInvoicedTotalAfterThisRun'
+)
+BEGIN
+
+                    IF OBJECT_ID(N'[dbo].[GetCurrentYearInvoicedTotalAfterThisRun]', 'FN') IS NOT NULL
+                        DROP FUNCTION [dbo].GetCurrentYearInvoicedTotalAfterThisRun
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260218155412_UpdateGetCurrentYearInvoicedTotalAfterThisRun'
+)
+BEGIN
+                    DECLARE @sql NVARCHAR(MAX)
+                    SET @sql = N'CREATE FUNCTION [dbo].[GetCurrentYearInvoicedTotalAfterThisRun] (
+                        @billingInstructionAcceptReject      VARCHAR(250),
+                        @suggestedBillingInstruction         VARCHAR(250),
+                        @currentYearInvoicedTotalToDate      DECIMAL(18,2),
+                        @invoiceAmount                       DECIMAL(18,2)
+                    )
+                    RETURNS DECIMAL(18,2)
+                    AS
+                    BEGIN
+                        -- Rule 1: Cancelled and Rejected instruction always returns last invoiced values
+                        IF @suggestedBillingInstruction = ''CANCEL'' AND @billingInstructionAcceptReject = ''Rejected''
+                             RETURN ISNULL(@currentYearInvoicedTotalToDate, 0);
+
+                        -- Rule 2: Cancelled instruction always returns NULL
+                        IF @suggestedBillingInstruction = ''CANCEL'' AND @billingInstructionAcceptReject = ''Accepted''
+                            RETURN NULL;
+
+                        -- Rule 3: Rejected INITIAL returns NULL
+                        IF @billingInstructionAcceptReject = ''Rejected'' AND @suggestedBillingInstruction = ''INITIAL''
+                            RETURN NULL;
+
+                        -- Rule 4: Rejected (but not INITIAL) returns current total as-is
+                        IF @billingInstructionAcceptReject = ''Rejected''
+                            RETURN ISNULL(@currentYearInvoicedTotalToDate, 0);
+
+                        -- Rule 5: Rebill replaces total with invoice amount
+                        IF @suggestedBillingInstruction = ''REBILL''
+                            RETURN ISNULL(@invoiceAmount, 0);
+
+                        -- Rule 6: Accepted or any other case adds invoice amount
+                        RETURN ISNULL(@currentYearInvoicedTotalToDate, 0) + ISNULL(@invoiceAmount, 0);
+                    END'
+                    EXEC(@sql)
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260218155412_UpdateGetCurrentYearInvoicedTotalAfterThisRun'
+)
+BEGIN
+    GRANT EXEC ON [dbo].[GetCurrentYearInvoicedTotalAfterThisRun] TO PUBLIC;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260218155412_UpdateGetCurrentYearInvoicedTotalAfterThisRun'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260218155412_UpdateGetCurrentYearInvoicedTotalAfterThisRun', N'8.0.7');
+END;
+GO
+
+COMMIT;
+GO
+
