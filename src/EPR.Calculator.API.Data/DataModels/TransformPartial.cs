8c9851e4571b1f53825d8ff1bd22f753cd2db500
Ashley Wright <ashley.wright@equalexperts.com>
2026-06-02 15:04:44 +0100

    ECV-546 - Add transform tables for billing run to avoid re-calc

---
+++ src/EPR.Calculator.API.Data/DataModels/TransformPartial.cs
@@ -0,0 +1,41 @@
+﻿namespace EPR.Calculator.API.Data.DataModels
+{  
+    public class TransformPartial
+    {
+        public int Id { get; set; }
+        public required int CalculatorRunId { get; set; }
+        public required int ProducerId { get; set; }
+        public string? SubsidiaryId { get; set; }
+        public string? ProducerName { get; set; }
+        public string? TradingName { get; set; }
+        public required string Level { get; set; }
+        public required int SubmissionYear { get; set; }
+        public required int DaysInSubmissionYear { get; set; }
+        public string? JoiningDate { get; set; }
+        public int? DaysObligated { get; set; }
+        public required decimal ObligatedFactor { get; set; }
+        public required string MaterialCode { get; set; }
+        public required decimal HouseholdTonnage { get; set; }
+        public decimal? HouseholdTonnageRed { get; set; }
+        public decimal? HouseholdTonnageAmber { get; set; }
+        public decimal? HouseholdTonnageGreen { get; set; }
+        public decimal? HouseholdTonnageRedMedical { get; set; }
+        public decimal? HouseholdTonnageAmberMedical { get; set; }
+        public decimal? HouseholdTonnageGreenMedical { get; set; }
+        public required decimal PublicBinTonnage { get; set; }
+        public decimal? PublicBinTonnageRed { get; set; }
+        public decimal? PublicBinTonnageAmber { get; set; }
+        public decimal? PublicBinTonnageGreen { get; set; }
+        public decimal? PublicBinTonnageRedMedical { get; set; }
+        public decimal? PublicBinTonnageAmberMedical { get; set; }
+        public decimal? PublicBinTonnageGreenMedical { get; set; }
+        public decimal? HDCTonnage { get; set; }
+        public decimal? HDCTonnageRed { get; set; }
+        public decimal? HDCTonnageAmber { get; set; }
+        public decimal? HDCTonnageGreen { get; set; }
+        public decimal? HDCTonnageRedMedical { get; set; }
+        public decimal? HDCTonnageAmberMedical { get; set; }
+        public decimal? HDCTonnageGreenMedical { get; set; }
+        public required decimal SMCWTonnage { get; set; }
+    }
+}
---
+++ src/EPR.Calculator.API.Data/TypeConfigurations/TransformProjectedH1Configuration.cs
@@ -0,0 +1,210 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using EPR.Calculator.API.Data.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EPR.Calculator.API.Data.TypeConfigurations
+{
+    [ExcludeFromCodeCoverage]
+    public class TransformProjectedH1Configuration : IEntityTypeConfiguration<TransformProjectedH1>
+    {
+        /// <inheritdoc />
+        public void Configure(EntityTypeBuilder<TransformProjectedH1> builder)
+        {
+              builder.ToTable("transform_projected_h1");
+
+              builder.HasIndex(x => x.CalculatorRunId);
+
+              builder.Property(p => p.Id)
+                     .HasColumnName("id")
+                     .IsRequired();
+              builder.Property(p => p.CalculatorRunId)
+                     .HasColumnName("calculator_run_id")
+                     .IsRequired();
+              builder.Property(p => p.ProducerId)
+                     .HasColumnName("producer_id")
+                     .IsRequired();
+              builder.Property(p => p.SubsidiaryId)
+                     .HasColumnName("subsidiary_id")
+                     .HasMaxLength(100)
+                     .IsRequired(false);
+              builder.Property(p => p.MaterialCode)
+                     .HasColumnName("material_code")
+                     .HasMaxLength(50);
+              builder.Property(p => p.SubmissionPeriodCode)
+                     .HasColumnName("submission_period")
+                     .HasMaxLength(50);
+              builder.Property(p => p.Level)
+                     .HasColumnName("level")
+                     .HasMaxLength(5);
+              builder.Property(p => p.HouseholdTonnage)
+                     .HasColumnName("household_tonnage")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageRed)
+                     .HasColumnName("household_tonnage_red")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageAmber)
+                     .HasColumnName("household_tonnage_amber")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageGreen)
+                     .HasColumnName("household_tonnage_green")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageRedMedical)
+                     .HasColumnName("household_tonnage_red_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageAmberMedical)
+                     .HasColumnName("household_tonnage_amber_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HouseholdTonnageGreenMedical)
+                     .HasColumnName("household_tonnage_green_medical")
+                     .HasPrecision(18, 3);       
+              builder.Property(p => p.PublicBinTonnage)
+                     .HasColumnName("public_bin_tonnage")
+                     .HasPrecision(18, 3);       
+              builder.Property(p => p.PublicBinTonnageRed)
+                     .HasColumnName("public_bin_tonnage_red")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageAmber)
+                     .HasColumnName("public_bin_tonnage_amber")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageGreen)
+                     .HasColumnName("public_bin_tonnage_green")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageRedMedical)
+                     .HasColumnName("public_bin_tonnage_red_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageAmberMedical)
+                     .HasColumnName("public_bin_tonnage_amber_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageGreenMedical)
+                     .HasColumnName("public_bin_tonnage_green_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HDCTonnage)
+                     .HasColumnName("hdc_tonnage")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageRed)
+                     .HasColumnName("hdc_tonnage_red")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageAmber)
+                     .HasColumnName("hdc_tonnage_amber")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageGreen)
+                     .HasColumnName("hdc_tonnage_green")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageRedMedical)
+                     .HasColumnName("hdc_tonnage_red_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageAmberMedical)
+                     .HasColumnName("hdc_tonnage_amber_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HDCTonnageGreenMedical)
+                     .HasColumnName("hdc_tonnage_green_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.HouseholdTonnageWithoutRAM)
+                     .HasColumnName("household_tonnage_without_ram")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.PublicBinTonnageWithoutRAM)
+                     .HasColumnName("public_bin_tonnage_without_ram")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.HDCTonnageWithoutRAM)
+                     .HasColumnName("hdc_tonnage_without_ram")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHouseholdTonnage)
+                     .HasColumnName("projected_household_tonnage")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageRed)
+                     .HasColumnName("projected_household_tonnage_red")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageAmber)
+                     .HasColumnName("projected_household_tonnage_amber")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageGreen)
+                     .HasColumnName("projected_household_tonnage_green")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageRedMedical)
+                     .HasColumnName("projected_household_tonnage_red_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageAmberMedical)
+                     .HasColumnName("projected_household_tonnage_amber_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHouseholdTonnageGreenMedical)
+                     .HasColumnName("projected_household_tonnage_green_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnage)
+                     .HasColumnName("projected_public_bin_tonnage")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageRed)
+                     .HasColumnName("projected_public_bin_tonnage_red")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageAmber)
+                     .HasColumnName("projected_public_bin_tonnage_amber")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageGreen)
+                     .HasColumnName("projected_public_bin_tonnage_green")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageRedMedical)
+                     .HasColumnName("projected_public_bin_tonnage_red_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageAmberMedical)
+                     .HasColumnName("projected_public_bin_tonnage_amber_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedPublicBinTonnageGreenMedical)
+                     .HasColumnName("projected_public_bin_tonnage_green_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.ProjectedHDCTonnage)
+                     .HasColumnName("projected_hdc_tonnage")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageRed)
+                     .HasColumnName("projected_hdc_tonnage_red")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageAmber)
+                     .HasColumnName("projected_hdc_tonnage_amber")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageGreen)
+                     .HasColumnName("projected_hdc_tonnage_green")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageRedMedical)
+                     .HasColumnName("projected_hdc_tonnage_red_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageAmberMedical)
+                     .HasColumnName("projected_hdc_tonnage_amber_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.ProjectedHDCTonnageGreenMedical)
+                     .HasColumnName("projected_hdc_tonnage_green_medical")
+                     .HasPrecision(18, 3)
+                     .IsRequired(false);
+              builder.Property(p => p.H2RamProportionsRed)
+                     .HasColumnName("h2_ram_proportions_red")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.H2RamProportionsAmber)
+                     .HasColumnName("h2_ram_proportions_amber")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.H2RamProportionsGreen)
+                     .HasColumnName("h2_ram_proportions_green")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.H2RamProportionsRedMedical)
+                     .HasColumnName("h2_ram_proportions_red_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.H2RamProportionsAmberMedical)
+                     .HasColumnName("h2_ram_proportions_amber_medical")
+                     .HasPrecision(18, 3);
+              builder.Property(p => p.H2RamProportionsGreenMedical)
+                     .HasColumnName("h2_ram_proportions_green_medical")
+                     .HasPrecision(18, 3);
+        }
+    }
+}
