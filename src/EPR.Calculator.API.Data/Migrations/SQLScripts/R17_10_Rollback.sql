371028256deae36bda29e66b24cc9bfcda1bdca7
Ashley Wright <ashley.wright@equalexperts.com>
2026-06-11 16:50:04 +0100

    ECV-510 - Update 17.10 rollback

--- src/EPR.Calculator.API.Data/Migrations/SQLScripts/R17_10_Rollback.sql
+++ src/EPR.Calculator.API.Data/Migrations/SQLScripts/R17_10_Rollback.sql
@@ -1,6 +1,208 @@
 ﻿BEGIN TRANSACTION;
 GO
 
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-AL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-FC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-GL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-OT';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-PC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-PL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-ST';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'ENG-WD';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-AL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-FC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-GL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-OT';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-PC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-PL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-ST';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'NI-WD';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-AL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-FC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-GL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-OT';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-PC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-PL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-ST';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'SCT-WD';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-AL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-FC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-GL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-OT';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-PC';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-PL';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-ST';
+SELECT @@ROWCOUNT;
+
+GO
+
+UPDATE [lapcap_data_template_master] SET [total_cost_from] = 0.0
+WHERE [unique_ref] = N'WLS-WD';
+SELECT @@ROWCOUNT;
+
+GO
+
+DELETE FROM [__EFMigrationsHistory]
+WHERE [MigrationId] = N'20260608135126_AllowNegativeLapCap';
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
 IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'relative_year', N'description') AND [object_id] = OBJECT_ID(N'[calculator_run_relative_years]'))
     SET IDENTITY_INSERT [calculator_run_relative_years] ON;
 INSERT INTO [calculator_run_relative_years] ([relative_year], [description])
