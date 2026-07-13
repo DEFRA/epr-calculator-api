f952723438429c53534d3e35390fa8785cadc71b
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-07-13 15:28:24 +0100

    refactor: Implements options pattern

---
+++ src/EPR.Calculator.API/Options/ServiceBusOptions.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EPR.Calculator.API.Options;
+
+[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+public record ServiceBusOptions
+{
+    public const string SectionKey = "ServiceBus";
+
+    [Required(AllowEmptyStrings = false)] public string ConnectionString { get; init; } = null!;
+    [Required(AllowEmptyStrings = false)] public string QueueName { get; init; } = null!;
+
+    [Required]
+    [Range(typeof(TimeSpan), "00:00:01", "00:01:00")]
+    public TimeSpan RetryDelay { get; init; } = TimeSpan.FromSeconds(3);
+
+    [Range(1, 100)] public int MaxRetries { get; init; } = 2;
+}
