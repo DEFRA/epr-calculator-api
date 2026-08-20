9776c91f405b6d8d534b220cce21156564828514
Mike McBain <mike.mcbain@esynergy.co.uk>
2026-08-20 14:13:19 +0100

    ECV-697: Reduces complexity of Program.cs

---
+++ src/EPR.Calculator.API/App/ServiceConfiguration.cs
@@ -0,0 +1,196 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Azure.Storage.Blobs;
+using EPR.Calculator.API.BackgroundService.Logging;
+using EPR.Calculator.API.BackgroundService.Services;
+using EPR.Calculator.API.BackgroundService.Telemetry;
+using EPR.Calculator.API.Data;
+using EPR.Calculator.API.Extensions;
+using EPR.Calculator.API.Filters;
+using EPR.Calculator.API.Options;
+using EPR.Calculator.API.Services;
+using EPR.Calculator.API.Validators;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
+
+namespace EPR.Calculator.API.App;
+
+[ExcludeFromCodeCoverage]
+public static class ServiceConfiguration
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddPayCalTelemetry(IConfiguration configuration, IHostEnvironment environment)
+        {
+            if (environment.IsLocal() && IsAppInsightsDisabled())
+                services.AddSingleton<ITelemetryClient, LoggerTelemetryClient>();
+            else
+            {
+                services.AddApplicationInsightsTelemetry();
+                services.AddSingleton<ITelemetryClient, AppInsightsTelemetryClient>();
+            }
+
+            return services;
+
+            bool IsAppInsightsDisabled()
+            {
+                var connectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")
+                                       ?? configuration["ApplicationInsights:ConnectionString"];
+
+                return string.IsNullOrWhiteSpace(connectionString)
+                       || connectionString.Contains($"InstrumentationKey={Guid.Empty}", StringComparison.OrdinalIgnoreCase);
+            }
+        }
+
+        public IServiceCollection AddPayCalProblemDetails(IHostEnvironment environment)
+        {
+            services.AddProblemDetails(options =>
+            {
+                if (environment.IsDevelopment() || environment.IsLocal())
+                {
+                    options.CustomizeProblemDetails = context =>
+                    {
+                        var exception = context.HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+                        if (exception is null)
+                            return;
+
+                        context.ProblemDetails.Detail = exception.Message;
+                        context.ProblemDetails.Extensions["exception"] = exception.ToString();
+                    };
+                }
+            });
+
+            return services;
+        }
+
+        public string AddPayCalCorsPolicy()
+        {
+            const string corsPolicy = "PayCalCors";
+
+            services.AddCors(options => options.AddPolicy(
+                corsPolicy,
+                policy => policy
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()));
+
+            return corsPolicy;
+        }
+
+        public IServiceCollection AddPayCalRequestValidation()
+        {
+            services
+                .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            services.Configure<MvcOptions>(options =>
+                options.Filters.Add<FluentValidationActionFilter>());
+
+            services.AddScoped<ICreateDefaultParameterDataValidator, CreateDefaultParameterDataValidator>();
+            services.AddScoped<ICalcRelativeYearRequestDtoDataValidator, CalcRelativeYearRequestDtoDataValidator>();
+            services.AddScoped<ICalculatorRunStatusDataValidator, CalculatorRunStatusDataValidator>();
+
+            return services;
+        }
+
+        public IServiceCollection AddPayCalAuthentication(IConfiguration configuration, IHostEnvironment environment)
+        {
+            var authenticationBuilder = services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme);
+            var entraIdConfig = configuration.GetSection("AzureAd");
+            var oidcConfig = configuration.GetSection("Oidc");
+
+            if (environment.IsLocal() && oidcConfig.GetValue<bool>("Enabled"))
+            {
+                authenticationBuilder.AddJwtBearer(options =>
+                {
+                    options.Authority = oidcConfig["Authority"];
+                    options.TokenValidationParameters.ValidateAudience = false;
+                });
+            }
+            else
+                authenticationBuilder.AddMicrosoftIdentityWebApi(entraIdConfig);
+
+            return services;
+        }
+
+        public IServiceCollection AddPayCalAuthorization()
+        {
+            services.AddAuthorizationBuilder()
+                .SetFallbackPolicy(new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .RequireRole("SASuperUser")
+                    .Build());
+
+            return services;
+        }
+
+        public IServiceCollection AddPayCalDatabase()
+        {
+            services
+                .AddOptions<DatabaseOptions>()
+                .BindConfiguration(DatabaseOptions.SectionKey)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            // When running on AspNetCore the factory defaults to creating contexts with Scoped lifetime.
+            services.AddDbContextFactory<ApplicationDBContext>((provider, builder) =>
+            {
+                var appOptions = provider
+                    .GetRequiredService<IOptions<DatabaseOptions>>()
+                    .Value;
+
+                builder.UseSqlServer(
+                    appOptions.ConnectionString,
+                    sqlOptions => { sqlOptions.CommandTimeout((int)appOptions.CommandTimeout.TotalSeconds); });
+            });
+
+            services.AddSingleton<IBulkOperations, BulkOperationsWrapper>();
+
+            return services;
+        }
+
+        public IServiceCollection AddPayCalBlobStorage()
+        {
+            services
+                .AddOptions<BlobStorageOptions>()
+                .BindConfiguration(BlobStorageOptions.SectionKey)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            services.AddAzureClients(builder =>
+            {
+                builder.AddClient<BlobServiceClient, BlobClientOptions>((_, _, provider) =>
+                {
+                    var appOptions = provider
+                        .GetRequiredService<IOptions<BlobStorageOptions>>()
+                        .Value;
+
+                    return new BlobServiceClient(appOptions.ConnectionString);
+                });
+            });
+
+            services.AddSingleton<IBlobStorageService, BlobStorageService>();
+
+            return services;
+        }
+
+        public IServiceCollection AddPayCalServices()
+        {
+            services.AddSingleton<TimeProvider>(_ => TimeProvider.System);
+
+            services.AddScoped<IInvoiceDetailsService, InvoiceDetailsService>();
+            services.AddScoped<IBillingFileService, BillingFileService>();
+            services.AddScoped<IAvailableClassificationsService, AvailableClassificationsService>();
+            services.AddScoped<ICalculationRunService, CalculationRunService>();
+
+            return services;
+        }
+    }
+}
---
+++ src/EPR.Calculator.API/GlobalSuppressions.cs
@@ -0,0 +1,3 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("SonarAnalyzer.CSharp", "S5122:Make sure this permissive CORS policy is safe here", Justification = "Pre-existing behaviour.")]
