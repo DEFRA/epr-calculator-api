6b75e84372157fd1cdf40f4d7e3ebcabb697df09
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2026-09-02 16:22:54 +0100

    ECV-730 Changed namespace and removed Response entities

--- src/EPR.Calculator.API.DataApi/CommonDataApi/Entities/PayCalOrganisation.cs
+++ src/EPR.Calculator.API.DataApi/CommonDataApi/Entities/PayCalOrganisation.cs
@@ -1,21 +1,22 @@
 using System.Diagnostics.CodeAnalysis;
 
-namespace EPR.CommonDataService.DataApi.App;
+namespace EPR.CommonDataService.DataApi.CommonDataApi.Entities;
 
 [ExcludeFromCodeCoverage]
-public sealed record OrganisationResponse
+public record PayCalOrganisation
 {
-    public int OrganisationId { get; init; }
+    public int? OrganisationId { get; init; }
     public string? SubsidiaryId { get; init; }
+    public string? SubmitterId { get; init; }
     public string? OrganisationName { get; init; }
     public string? TradingName { get; init; }
     public string? StatusCode { get; init; }
-    public string? ErrorCode { get; init; }
-    public string? JoinerDate { get; init; }
     public string? LeaverDate { get; init; }
+    public string? JoinerDate { get; init; }
     public string? ObligationStatus { get; init; }
     public short? NumDaysObligated { get; init; }
-    public string? SubmitterId { get; init; }
+    public string? ErrorCode { get; init; }
+    public int? SubmissionPeriodYear { get; init; }
     public bool HasH1 { get; init; }
     public bool HasH2 { get; init; }
 }
